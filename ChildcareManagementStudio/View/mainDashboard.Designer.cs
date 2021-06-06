8b11e4bae5f1e559990cfa81ad1b8ca814f36538
J. Allen Burton <jburton6@my.westga.edu>
2021-06-05 21:37:00 -0400

    Added tab indicator and logout icon

--- ChildcareManagementStudio/View/mainDashboard.Designer.cs
+++ ChildcareManagementStudio/View/mainDashboard.Designer.cs
@@ -31,21 +31,25 @@ namespace ChildcareManagementStudio.View
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainDashboard));
             this.panelTop = new System.Windows.Forms.Panel();
-            this.panelLeftSide = new System.Windows.Forms.Panel();
-            this.labelTitle = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.buttonTeacherTab = new System.Windows.Forms.Button();
-            this.buttonStudentsTab = new System.Windows.Forms.Button();
-            this.buttonClassroomsTab = new System.Windows.Forms.Button();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelLeftSide = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.buttonFinancialTab = new System.Windows.Forms.Button();
+            this.buttonClassroomsTab = new System.Windows.Forms.Button();
+            this.buttonStudentsTab = new System.Windows.Forms.Button();
+            this.buttonTeacherTab = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panelTop.SuspendLayout();
-            this.panelLeftSide.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelLeftSide.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panelTop
             // 
             this.panelTop.BackColor = System.Drawing.Color.Maroon;
+            this.panelTop.Controls.Add(this.pictureBox2);
             this.panelTop.Controls.Add(this.pictureBox1);
             this.panelTop.Controls.Add(this.labelTitle);
             this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
@@ -54,18 +58,15 @@ namespace ChildcareManagementStudio.View
             this.panelTop.Size = new System.Drawing.Size(1045, 78);
             this.panelTop.TabIndex = 0;
             // 
-            // panelLeftSide
+            // pictureBox1
             // 
-            this.panelLeftSide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.panelLeftSide.Controls.Add(this.buttonFinancialTab);
-            this.panelLeftSide.Controls.Add(this.buttonClassroomsTab);
-            this.panelLeftSide.Controls.Add(this.buttonStudentsTab);
-            this.panelLeftSide.Controls.Add(this.buttonTeacherTab);
-            this.panelLeftSide.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLeftSide.Location = new System.Drawing.Point(0, 78);
-            this.panelLeftSide.Name = "panelLeftSide";
-            this.panelLeftSide.Size = new System.Drawing.Size(244, 589);
-            this.panelLeftSide.TabIndex = 1;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(152, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
             // 
             // labelTitle
             // 
@@ -78,63 +79,85 @@ namespace ChildcareManagementStudio.View
             this.labelTitle.TabIndex = 0;
             this.labelTitle.Text = "Childcare Management Studio";
             // 
-            // pictureBox1
+            // panelLeftSide
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(152, 8);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(66, 64);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.panelLeftSide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelLeftSide.Controls.Add(this.panel1);
+            this.panelLeftSide.Controls.Add(this.buttonFinancialTab);
+            this.panelLeftSide.Controls.Add(this.buttonClassroomsTab);
+            this.panelLeftSide.Controls.Add(this.buttonStudentsTab);
+            this.panelLeftSide.Controls.Add(this.buttonTeacherTab);
+            this.panelLeftSide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeftSide.Location = new System.Drawing.Point(0, 78);
+            this.panelLeftSide.Name = "panelLeftSide";
+            this.panelLeftSide.Size = new System.Drawing.Size(266, 589);
+            this.panelLeftSide.TabIndex = 1;
             // 
-            // buttonTeacherTab
+            // panel1
             // 
-            this.buttonTeacherTab.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonTeacherTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonTeacherTab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.buttonTeacherTab.Location = new System.Drawing.Point(3, 6);
-            this.buttonTeacherTab.Name = "buttonTeacherTab";
-            this.buttonTeacherTab.Size = new System.Drawing.Size(235, 140);
-            this.buttonTeacherTab.TabIndex = 0;
-            this.buttonTeacherTab.Text = "Teachers";
-            this.buttonTeacherTab.UseVisualStyleBackColor = true;
-            // 
-            // buttonStudentsTab
-            // 
-            this.buttonStudentsTab.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonStudentsTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonStudentsTab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.buttonStudentsTab.Location = new System.Drawing.Point(3, 152);
-            this.buttonStudentsTab.Name = "buttonStudentsTab";
-            this.buttonStudentsTab.Size = new System.Drawing.Size(235, 140);
-            this.buttonStudentsTab.TabIndex = 1;
-            this.buttonStudentsTab.Text = "Students";
-            this.buttonStudentsTab.UseVisualStyleBackColor = true;
-            // 
-            // buttonClassroomsTab
-            // 
-            this.buttonClassroomsTab.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonClassroomsTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonClassroomsTab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.buttonClassroomsTab.Location = new System.Drawing.Point(3, 298);
-            this.buttonClassroomsTab.Name = "buttonClassroomsTab";
-            this.buttonClassroomsTab.Size = new System.Drawing.Size(235, 140);
-            this.buttonClassroomsTab.TabIndex = 2;
-            this.buttonClassroomsTab.Text = "Classrooms";
-            this.buttonClassroomsTab.UseVisualStyleBackColor = true;
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(0, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 140);
+            this.panel1.TabIndex = 2;
             // 
             // buttonFinancialTab
             // 
             this.buttonFinancialTab.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonFinancialTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonFinancialTab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.buttonFinancialTab.Location = new System.Drawing.Point(3, 446);
+            this.buttonFinancialTab.Location = new System.Drawing.Point(12, 446);
             this.buttonFinancialTab.Name = "buttonFinancialTab";
-            this.buttonFinancialTab.Size = new System.Drawing.Size(235, 140);
+            this.buttonFinancialTab.Size = new System.Drawing.Size(251, 140);
             this.buttonFinancialTab.TabIndex = 3;
             this.buttonFinancialTab.Text = "Financial";
             this.buttonFinancialTab.UseVisualStyleBackColor = true;
+            // 
+            // buttonClassroomsTab
+            // 
+            this.buttonClassroomsTab.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClassroomsTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClassroomsTab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonClassroomsTab.Location = new System.Drawing.Point(12, 298);
+            this.buttonClassroomsTab.Name = "buttonClassroomsTab";
+            this.buttonClassroomsTab.Size = new System.Drawing.Size(251, 140);
+            this.buttonClassroomsTab.TabIndex = 2;
+            this.buttonClassroomsTab.Text = "Classrooms";
+            this.buttonClassroomsTab.UseVisualStyleBackColor = true;
+            // 
+            // buttonStudentsTab
+            // 
+            this.buttonStudentsTab.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStudentsTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStudentsTab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonStudentsTab.Location = new System.Drawing.Point(12, 152);
+            this.buttonStudentsTab.Name = "buttonStudentsTab";
+            this.buttonStudentsTab.Size = new System.Drawing.Size(251, 140);
+            this.buttonStudentsTab.TabIndex = 1;
+            this.buttonStudentsTab.Text = "Students";
+            this.buttonStudentsTab.UseVisualStyleBackColor = true;
+            // 
+            // buttonTeacherTab
+            // 
+            this.buttonTeacherTab.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonTeacherTab.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTeacherTab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.buttonTeacherTab.Location = new System.Drawing.Point(12, 6);
+            this.buttonTeacherTab.Name = "buttonTeacherTab";
+            this.buttonTeacherTab.Size = new System.Drawing.Size(251, 140);
+            this.buttonTeacherTab.TabIndex = 0;
+            this.buttonTeacherTab.Text = "Teachers";
+            this.buttonTeacherTab.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(997, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 37);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
             // 
             // mainDashboard
             // 
@@ -149,8 +172,9 @@ namespace ChildcareManagementStudio.View
             this.Text = "Form1";
             this.panelTop.ResumeLayout(false);
             this.panelTop.PerformLayout();
-            this.panelLeftSide.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelLeftSide.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -165,5 +189,7 @@ namespace ChildcareManagementStudio.View
         private System.Windows.Forms.Button buttonClassroomsTab;
         private System.Windows.Forms.Button buttonStudentsTab;
         private System.Windows.Forms.Button buttonTeacherTab;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
