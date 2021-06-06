7c0acbcf44061f0d0b15820f174dcbaf4033075d
Michael Studard <mstudar1@my.westga.edu>
2021-06-05 21:14:31 -0400

    Added Employee class with auto-implemented properties

---
+++ ChildcareManagementStudio/Model/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class representing an employee.
+    /// </summary>
+    public class Employee : Person
+    {
+        public int EmployeeId { get; set; }
+
+        public DateTime startDate { get; set; }
+    }
+}
