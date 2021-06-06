6fc165091f93b62d01e92f8f09b7fdc85961b74b
Michael Studard <mstudar1@my.westga.edu>
2021-06-06 16:06:30 -0400

    Added ChildCareDatabaseConnection class

---
+++ ChildcareManagementStudio/DAL/ChildCareDatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace ChildcareManagementStudio.DAL
+{
+    /// <summary>
+    /// Class that can be used to get a connection to the child care database.
+    /// </summary>
+    public static class ChildCareDatabaseConnection
+    {
+        /// <summary>
+        /// Method that can be used to get a connection to the child care database.
+        /// </summary>
+        /// <returns></returns>
+        public static SqliteConnection GetConnection()
+        {
+            string connectionString = @"Data Source=.\ChildCareData.db;";
+            SqliteConnection connection = new SqliteConnection(connectionString);
+            return connection;
+        }
+    }
+}
