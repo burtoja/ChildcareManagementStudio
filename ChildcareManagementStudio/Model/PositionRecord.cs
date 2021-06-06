08baed8e828375bf39c429686a3c2521f67d2fde
Michael Studard <mstudar1@my.westga.edu>
2021-06-06 15:44:33 -0400

    Added Position Record class with auto-implemented properties

---
+++ ChildcareManagementStudio/Model/PositionRecord.cs
@@ -0,0 +1,18 @@
+﻿namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class for storing position records for an employee.
+    /// </summary>
+    public class PositionRecord
+    {
+        /// <summary>
+        /// Property for the name of the position.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Property for the school year when the employee held the specified position.
+        /// </summary>
+        public string SchoolYear { get; set; }
+    }
+}
