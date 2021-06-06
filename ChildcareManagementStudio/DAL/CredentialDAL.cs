31a392435f095d35479d7b174aff62639ec39fad
Michael Studard <mstudar1@my.westga.edu>
2021-06-06 16:53:25 -0400

    Added CredentialDAL class with CredentialsAreValid method

---
+++ ChildcareManagementStudio/DAL/CredentialDAL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Data.Sqlite;
+
+namespace ChildcareManagementStudio.DAL
+{
+    /// <summary>
+    /// Class for accessing credential information from the child care database.
+    /// </summary>
+    public class CredentialDAL
+    {
+        /// <summary>
+        /// Method for checking if the specified credentials are valid.
+        /// </summary>
+        /// <param name="username">The username being checked.</param>
+        /// <param name="passwordHash">The sha256 password hash being checked.</param>
+        /// <returns>True if the specified credentials are valid.  False if the specified credentials are invalid.</returns>
+        public bool CredentialsAreValid(string username, string passwordHash)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new ArgumentNullException("username", "The username cannot be null or empty.");
+            }
+
+            using (var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(""));
+                string emptyStringHash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+                if (passwordHash == emptyStringHash)
+                {
+                    throw new ArgumentNullException("password", "The password cannot be null or empty.");
+                }
+            }
+
+            string selectStatement =
+                "SELECT username " +
+                "FROM Credential " +
+                "WHERE username = $username " +
+                "AND password = $passwordHash";
+
+            using (SqliteConnection connection = ChildCareDatabaseConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqliteCommand selectCommand = new SqliteCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("$username", username);
+                    selectCommand.Parameters.AddWithValue("$passwordHash", passwordHash);
+                    using (SqliteDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        return reader.HasRows;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ChildcareManagementStudioTests/DAL/CredentialDALWhenCheckCredentialsAreValid.cs
@@ -0,0 +1,54 @@
+﻿using ChildcareManagementStudio.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChildcareManagementStudioTests.DAL
+{
+    /// <summary>
+    /// Class for testing the CredentialsAreValid method of the CredentialDAL class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class CredentialDALWhenCheckCredentialsAreValid
+    {
+        private CredentialDAL credentialDAL;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            credentialDAL = new CredentialDAL();
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfUsernameIsBlank()
+        {
+            string username = "";
+            string password = "33c614ca3cf78827a85dc0d8d06bfcf8c4d923fd23c813acd50b80ed2d4d4fb3";
+            Assert.ThrowsException<System.ArgumentNullException>(() => credentialDAL.CredentialsAreValid(username, password));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfPasswordIsBlank()
+        {
+            string username = "mmouse";
+            string password = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+            Assert.ThrowsException<System.ArgumentNullException>(() => credentialDAL.CredentialsAreValid(username, password));
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrueWhenCredentialsAreValid()
+        {
+            string username = "mmouse";
+            string password = "33c614ca3cf78827a85dc0d8d06bfcf8c4d923fd23c813acd50b80ed2d4d4fb3";
+            Assert.AreEqual(true, credentialDAL.CredentialsAreValid(username, password));
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseWhenCredentialsAreInvalid()
+        {
+            string username = "mmouse";
+            string password = "mouse";
+            Assert.AreEqual(false, credentialDAL.CredentialsAreValid(username, password));
+        }
+    }
+}
