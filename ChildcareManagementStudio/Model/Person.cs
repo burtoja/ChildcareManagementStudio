aac51a340014d4d5b02bf79943ed1594a2e376c6
Michael Studard <mstudar1@my.westga.edu>
2021-06-05 22:01:07 -0400

    Added input validation for the SocialSecurityNumber property of the Person class

--- ChildcareManagementStudio/Model/Person.cs
+++ ChildcareManagementStudio/Model/Person.cs
@@ -7,6 +7,8 @@ namespace ChildcareManagementStudio.Model
     /// </summary>
     public class Person
     {
+        private string socialSecurityNumber;
+
         /// <summary>
         /// Property for a person's unique person ID.
         /// </summary>
@@ -30,7 +32,21 @@ namespace ChildcareManagementStudio.Model
         /// <summary>
         /// Property for a person's social security number.
         /// </summary>
-        public string SocialSecurityNumber { get; set; }
+        public string SocialSecurityNumber
+        {
+            get
+            {
+                return socialSecurityNumber;
+            }
+            set
+            {
+                if (!IsValidSocialSecurityNumber(value))
+                {
+                    throw new ArgumentException("The specified social security number is not formatted properly.  A format of ###-##-#### must be used.");
+                }
+                socialSecurityNumber = value;
+            }
+        }
 
         /// <summary>
         /// Property for a person's gender.
@@ -76,6 +92,19 @@ namespace ChildcareManagementStudio.Model
             {
                 return FirstName + " " + LastName;
             }
+        }
+
+        /// <summary>
+        /// Helper method that indicates if a social security number is formatted properly.
+        /// </summary>
+        /// <param name="socialSecurityNumber">The social security number being evaluated.</param>
+        /// <returns>
+        /// True if the social security number is formatted properly (e.g., "123-45-6789").
+        /// False if the social security number is not formatted properly.
+        /// </returns>
+        private bool IsValidSocialSecurityNumber(string socialSecurityNumber)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(socialSecurityNumber, @"^\d{3}-\d{2}-\d{4}$");
         }
     }
 }
---
+++ ChildcareManagementStudioTests/PersonWhenSetSocialSecurityNumber.cs
@@ -0,0 +1,81 @@
+﻿using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChildcareManagementStudioTests
+{
+    /// <summary>
+    /// Class for testing the input validation for the SocialSecurityNumber property of the Person class.
+    /// </summary>
+    [TestClass]
+    public class PersonWhenSetSocialSecurityNumber
+    {
+        private Person person;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            person = new Person();
+        }
+
+        [TestMethod]
+        public void ShouldNotAllowBlankSocialSecurityNumber()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => person.SocialSecurityNumber = "");
+        }
+
+        [TestMethod]
+        public void ShouldNotAllowSocialSecurityNumberWithoutDashes()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => person.SocialSecurityNumber = "123456789");
+        }
+
+        [TestMethod]
+        public void ShouldNotAllowSocialSecurityNumberWithTooFewDigitsInFirstSection()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => person.SocialSecurityNumber = "12-45-6789");
+        }
+
+        [TestMethod]
+        public void ShouldNotAllowSocialSecurityNumberWithTooManyDigitsInFirstSection()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => person.SocialSecurityNumber = "1234-45-6789");
+        }
+
+        [TestMethod]
+        public void ShouldNotAllowSocialSecurityNumberWithTooFewDigitsInSecondSection()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => person.SocialSecurityNumber = "123-4-6789");
+        }
+
+        [TestMethod]
+        public void ShouldNotAllowSocialSecurityNumberWithTooManyDigitsInSecondSection()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => person.SocialSecurityNumber = "123-456-6789");
+        }
+
+        [TestMethod]
+        public void ShouldNotAllowSocialSecurityNumberWithTooFewDigitsInThirdSection()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => person.SocialSecurityNumber = "123-45-678");
+        }
+
+        [TestMethod]
+        public void ShouldNotAllowSocialSecurityNumberWithTooManyDigitsInThirdSection()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => person.SocialSecurityNumber = "123-45-67890");
+        }
+
+        [TestMethod]
+        public void ShouldNotAllowSocialSecurityNumberWithCharactersThatAreNotNumbers()
+        {
+            Assert.ThrowsException<System.ArgumentException>(() => person.SocialSecurityNumber = "O23-45-6789");
+        }
+
+        [TestMethod]
+        public void ShouldSetWhenSocialSecurityNumberIsValid()
+        {
+            person.SocialSecurityNumber = "123-45-6789";
+            Assert.AreEqual("123-45-6789", person.SocialSecurityNumber);
+        }
+    }
+}
