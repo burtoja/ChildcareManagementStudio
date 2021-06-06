fd86abcd1087370f200f422368e3df51b9504fd7
Michael Studard <mstudar1@my.westga.edu>
2021-06-06 15:48:44 -0400

    Added CertificationRecord class with auto-implemented properties

---
+++ ChildcareManagementStudio/Model/CertificationRecord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class for storing certification records for an employee.
+    /// </summary>
+    public class CertificationRecord
+    {
+        /// <summary>
+        /// Property for the name of the certification.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Property for the expiration date of the certification.
+        /// </summary>
+        public DateTime ExpirationDate { get; set; }
+    }
+}
