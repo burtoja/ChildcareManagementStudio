ff420911f116f2f657ddcf238f56df7b2d20e8e2
Michael Studard <mstudar1@my.westga.edu>
2021-06-06 18:16:54 -0400

    Added CertificationDAL class with GetCertificationRecords method

---
+++ ChildcareManagementStudio/DAL/CertificationDAL.cs
@@ -0,0 +1,61 @@
+﻿using ChildcareManagementStudio.Model;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudio.DAL
+{
+    /// <summary>
+    /// Class for accessing certification information from the child care database.
+    /// </summary>
+    public class CertificationDAL
+    {
+        /// <summary>
+        /// Method that returns all of the certification records for a given employee.
+        /// </summary>
+        /// <param name="employeeId">Employee ID for the employee in question.</param>
+        /// <returns>A list of all the certification records for the specified employee.</returns>
+        public List<CertificationRecord> GetCertificationRecords(int employeeId)
+        {
+            if (employeeId < 0)
+            {
+                throw new ArgumentException("The employee ID cannot be a negative number.", "employeeId");
+            }
+
+            List<CertificationRecord> certificationRecords = new List<CertificationRecord>();
+
+            string selectStatement =
+                "SELECT type, expirationDate " +
+                "FROM Certification " +
+                "WHERE employeeId = $employeeId " +
+                "ORDER BY expirationDate";
+
+            using (SqliteConnection connection = ChildCareDatabaseConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqliteCommand selectCommand = new SqliteCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("$employeeId", employeeId);
+                    using (SqliteDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        int typeOrdinal = reader.GetOrdinal("type");
+                        int expirationDateOrdinal = reader.GetOrdinal("expirationDate");
+                        while (reader.Read())
+                        {
+                            string type = reader.GetString(typeOrdinal);
+                            DateTime expirationDate = reader.GetDateTime(expirationDateOrdinal);
+                            CertificationRecord currentCertificationRecord = new CertificationRecord
+                            {
+                                Type = type,
+                                ExpirationDate = expirationDate
+                            };
+                            certificationRecords.Add(currentCertificationRecord);
+                        }
+                    }
+                }
+            }
+
+            return certificationRecords;
+        }
+    }
+}
---
+++ ChildcareManagementStudioTests/DAL/CertificationDALWhenGetCertificationRecords.cs
@@ -0,0 +1,50 @@
+﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudioTests.DAL
+{
+    /// <summary>
+    /// Class for testing the GetCertificationRecords method of the CertificationDAL class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class CertificationDALWhenGetCertificationRecords
+    {
+        private CertificationDAL certificationDAL;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            certificationDAL = new CertificationDAL();
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfEmployeeIdIsNegative()
+        {
+            int employeeId = -1;
+            Assert.ThrowsException<System.ArgumentException>(() => certificationDAL.GetCertificationRecords(employeeId));
+        }
+
+        [TestMethod]
+        public void ShouldReturnEmptyListIfRecordsDoNotExist()
+        {
+            int employeeId = 0;
+            List<CertificationRecord> certificationRecords = certificationDAL.GetCertificationRecords(employeeId);
+            Assert.AreEqual(0, certificationRecords.Count);
+        }
+
+        [TestMethod]
+        public void ShouldReturnRecordsIfTheyExist()
+        {
+            int employeeId = 1;
+            List<CertificationRecord> certificationRecords = certificationDAL.GetCertificationRecords(employeeId);
+            Assert.AreEqual(1, certificationRecords.Count);
+            Assert.AreEqual("FCCPC", certificationRecords[0].Type);
+            Assert.AreEqual(new DateTime(2021, 12, 31), certificationRecords[0].ExpirationDate);
+        }
+    }
+}
