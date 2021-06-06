d80697abef396781429d99289c816bc40b5044f9
Michael Studard <mstudar1@my.westga.edu>
2021-06-06 17:41:08 -0400

    Added SalaryDAL class with GetSalaryRecords method

---
+++ ChildcareManagementStudio/DAL/SalaryDAL.cs
@@ -0,0 +1,61 @@
+﻿using ChildcareManagementStudio.Model;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudio.DAL
+{
+    /// <summary>
+    /// Class for accessing salary information from the child care database.
+    /// </summary>
+    public class SalaryDAL
+    {
+        /// <summary>
+        /// Method that returns all of the salary records for a given employee.
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <returns></returns>
+        public List<SalaryRecord> GetSalaryRecords(int employeeId)
+        {
+            if (employeeId < 0)
+            {
+                throw new ArgumentException("The employee ID cannot be a negative number.", "employeeId");
+            }
+
+            List<SalaryRecord> salaryRecords = new List<SalaryRecord>();
+
+            string selectStatement =
+                "SELECT effectiveDate, rate " +
+                "FROM Salary " +
+                "WHERE employeeId = $employeeId " +
+                "ORDER BY effectiveDate";
+
+            using (SqliteConnection connection = ChildCareDatabaseConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqliteCommand selectCommand = new SqliteCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("$employeeId", employeeId);
+                    using (SqliteDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        int effectiveDateOrdinal = reader.GetOrdinal("effectiveDate");
+                        int rateOrdinal = reader.GetOrdinal("rate");
+                        while (reader.Read())
+                        {
+                            DateTime effectiveDate = reader.GetDateTime(effectiveDateOrdinal);
+                            double rate = reader.GetDouble(rateOrdinal);
+                            SalaryRecord currentSalaryRecord = new SalaryRecord
+                            {
+                                EffectiveDate = effectiveDate,
+                                Rate = rate
+                            };
+                            salaryRecords.Add(currentSalaryRecord);
+                        }
+                    }
+                }
+            }
+
+            return salaryRecords;
+        }
+    }
+}
---
+++ ChildcareManagementStudioTests/DAL/CredentialDALWhenGetSalaryRecords.cs
@@ -0,0 +1,50 @@
+﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudioTests.DAL
+{
+    /// <summary>
+    /// Class for testing the GetSalaryRecords method of the SalaryDAL class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class CredentialDALWhenGetSalaryRecords
+    {
+        private SalaryDAL salaryDAL;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            salaryDAL = new SalaryDAL();
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfEmployeeIdIsNegative()
+        {
+            int employeeId = -1;
+            Assert.ThrowsException<System.ArgumentException>(() => salaryDAL.GetSalaryRecords(employeeId));
+        }
+
+        [TestMethod]
+        public void ShouldReturnEmptyListIfRecordsDoNotExist()
+        {
+            int employeeId = 0;
+            List<SalaryRecord> salaryRecords = salaryDAL.GetSalaryRecords(employeeId);
+            Assert.AreEqual(0, salaryRecords.Count);
+        }
+
+        [TestMethod]
+        public void ShouldReturnRecordsIfTheyExist()
+        {
+            int employeeId = 1;
+            List<SalaryRecord> salaryRecords = salaryDAL.GetSalaryRecords(employeeId);
+            Assert.AreEqual(1, salaryRecords.Count);
+            Assert.AreEqual(new DateTime(2021, 6, 5), salaryRecords[0].EffectiveDate);
+            Assert.AreEqual(10.00, salaryRecords[0].Rate);
+        }
+    }
+}
