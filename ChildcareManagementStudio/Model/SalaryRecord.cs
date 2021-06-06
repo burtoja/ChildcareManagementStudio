33410923f50c8dcbcc635bd04c7efa101c96adb9
Michael Studard <mstudar1@my.westga.edu>
2021-06-06 15:53:17 -0400

    Added SalaryRecord class with auto-implemented properties

---
+++ ChildcareManagementStudio/Model/SalaryRecord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class for storing salary records for an employee.
+    /// </summary>
+    public class SalaryRecord
+    {
+        /// <summary>
+        /// Property for the date when the specified salary took effect.
+        /// </summary>
+        public DateTime EffectiveDate { get; set; }
+
+        /// <summary>
+        /// Property for the hourly pay rate (in US dollars).
+        /// </summary>
+        public double Rate { get; set; }
+    }
+}
