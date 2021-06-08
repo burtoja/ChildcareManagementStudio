569f283ce11f6b9fd1abba829eef4420dcf1538f
J. Allen Burton <jburton6@my.westga.edu>
2021-06-08 18:12:53 -0400

    List view item details added.

--- ChildcareManagementStudio/UserControls/TeacherViewTeachersUserControl.Designer.cs
+++ ChildcareManagementStudio/UserControls/TeacherViewTeachersUserControl.Designer.cs
@@ -30,16 +30,52 @@ namespace ChildcareManagementStudio.UserControls
         private void InitializeComponent()
         {
             this.listViewAllTeachers = new System.Windows.Forms.ListView();
+            this.lastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.firstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dob = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.phone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // listViewAllTeachers
             // 
+            this.listViewAllTeachers.AccessibleDescription = "List of all teacher";
+            this.listViewAllTeachers.AccessibleName = "Teacher List";
+            this.listViewAllTeachers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lastName,
+            this.firstName,
+            this.dob,
+            this.phone});
+            this.listViewAllTeachers.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listViewAllTeachers.FullRowSelect = true;
+            this.listViewAllTeachers.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.listViewAllTeachers.HideSelection = false;
-            this.listViewAllTeachers.Location = new System.Drawing.Point(0, 19);
+            this.listViewAllTeachers.Location = new System.Drawing.Point(16, 21);
+            this.listViewAllTeachers.MultiSelect = false;
             this.listViewAllTeachers.Name = "listViewAllTeachers";
-            this.listViewAllTeachers.Size = new System.Drawing.Size(742, 385);
+            this.listViewAllTeachers.Size = new System.Drawing.Size(708, 383);
             this.listViewAllTeachers.TabIndex = 0;
             this.listViewAllTeachers.UseCompatibleStateImageBehavior = false;
+            this.listViewAllTeachers.View = System.Windows.Forms.View.Details;
+            // 
+            // lastName
+            // 
+            this.lastName.Text = "Last Name";
+            this.lastName.Width = 200;
+            // 
+            // firstName
+            // 
+            this.firstName.Text = "First Name";
+            this.firstName.Width = 200;
+            // 
+            // dob
+            // 
+            this.dob.Text = "Date of Birth";
+            this.dob.Width = 130;
+            // 
+            // phone
+            // 
+            this.phone.Text = "Phone Number";
+            this.phone.Width = 180;
             // 
             // TeacherViewTeachersUserControl
             // 
@@ -55,5 +91,9 @@ namespace ChildcareManagementStudio.UserControls
         #endregion
 
         private System.Windows.Forms.ListView listViewAllTeachers;
+        private System.Windows.Forms.ColumnHeader lastName;
+        private System.Windows.Forms.ColumnHeader firstName;
+        private System.Windows.Forms.ColumnHeader dob;
+        private System.Windows.Forms.ColumnHeader phone;
     }
 }
--- ChildcareManagementStudio/UserControls/TeacherViewTeachersUserControl.cs
+++ ChildcareManagementStudio/UserControls/TeacherViewTeachersUserControl.cs
@@ -8,13 +8,14 @@ namespace ChildcareManagementStudio.UserControls
 {
     public partial class TeacherViewTeachersUserControl : UserControl
     {
-
         private readonly EmployeeController employeeController;
+        private readonly PersonController personController;
 
         public TeacherViewTeachersUserControl()
         {
             InitializeComponent();
             this.employeeController = new EmployeeController();
+            this.personController = new PersonController();
             this.PopulateListView();
         }
 
@@ -23,10 +24,18 @@ namespace ChildcareManagementStudio.UserControls
             List<Employee> employeeList = this.employeeController.GetAllEmployees();
             foreach (Employee current in employeeList)
             {
-                ListViewItem item = new ListViewItem(current.EmployeeId.ToString());
-                item.SubItems.Add("Placeholder Name for Testing");
+                Person person = this.GetPerson(current);
+                ListViewItem item = new ListViewItem(person.LastName.ToString());
+                item.SubItems.Add(person.FirstName.ToString());
+                item.SubItems.Add(person.DateOfBirth.ToString());
+                item.SubItems.Add(person.PhoneNumber.ToString());
                 this.listViewAllTeachers.Items.Add(item);
             }
+        }
+
+        private  Person GetPerson(Employee theEmployee)
+        {
+            return this.personController.GetPerson(theEmployee.PersonId);
         }
 
     }
