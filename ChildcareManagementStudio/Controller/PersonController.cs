759d00734fc2af6b602fcb0617d79488730a1392
Michael Studard <mstudar1@my.westga.edu>
2021-06-07 20:34:16 -0400

    Added AddPerson method to PersonController class

--- ChildcareManagementStudio/Controller/PersonController.cs
+++ ChildcareManagementStudio/Controller/PersonController.cs
@@ -33,5 +33,39 @@ namespace ChildcareManagementStudio.Controller
 
             return personDAL.GetPerson(personId);
         }
+
+        /// <summary>
+        /// Method that adds a person to the database.
+        /// </summary>
+        /// <param name="person">
+        /// A Person object representing the person to be added to the database.
+        /// For the fields in the database that are set as not null, the object must have corresponding property values.
+        /// Additionally, the Person object being passed to this method cannot have a value for the PersonId property since it is assigned by the database.
+        /// This method completes the PersonId property for the referenced Person object after it is assigned by the database.
+        /// </param>
+        public void AddPerson(Person person)
+        {
+            if (PersonIsMissingMandatoryFields(person))
+            {
+                throw new ArgumentException("The Person object must have all the mandatory properties filled out.", "person");
+            }
+
+            if (person.PersonId != default)
+            {
+                throw new ArgumentException("The PersonId property cannot be filled out because it will be assigned by the database.", "person");
+            }
+
+            personDAL.AddPerson(person);
+        }
+
+        /// <summary>
+        /// Helper method that ensures all of the mandatory properties are included with the specified Person object.
+        /// </summary>
+        /// <param name="person">The Person object being evaluated.</param>
+        /// <returns>True if the object is missing mandatory properties, false otherwise.</returns>
+        private bool PersonIsMissingMandatoryFields(Person person)
+        {
+            return person.LastName == default || person.FirstName == default || person.DateOfBirth == default || person.SocialSecurityNumber == default || person.Gender == default || person.PhoneNumber == default || person.PhoneNumber == default || person.AddressLine1 == default || person.City == default || person.State == default || person.ZipCode == default;
+        }
     }
 }
---
+++ ChildcareManagementStudioTests/Controller/PersonControllerWhenAddPerson.cs
@@ -0,0 +1,98 @@
+﻿using ChildcareManagementStudio.Controller;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ChildcareManagementStudioTests.Controller
+{
+    /// <summary>
+    /// Class for testing the AddPerson method of the PersonController class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class PersonControllerWhenAddPerson
+    {
+        private PersonController personController;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            personController = new PersonController();
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfPersonIsMissingMandatoryProperty()
+        {
+            Person personToAdd = new Person
+            {
+                FirstName = "Donald",
+                DateOfBirth = new DateTime(1950, 1, 1),
+                SocialSecurityNumber = "333-33-3333",
+                Gender = "M",
+                PhoneNumber = "(333) 333-3333",
+                AddressLine1 = "7550 Maple Street",
+                City = "Orlando",
+                State = "FL",
+                ZipCode = "32789"
+            };
+
+            Assert.ThrowsException<System.ArgumentException>(() => personController.AddPerson(personToAdd));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfPersonAlreadyHasPersonId()
+        {
+            Person personToAdd = new Person
+            {
+                PersonId = 3,
+                LastName = "Duck",
+                FirstName = "Donald",
+                DateOfBirth = new DateTime(1950, 1, 1),
+                SocialSecurityNumber = "333-33-3333",
+                Gender = "M",
+                PhoneNumber = "(333) 333-3333",
+                AddressLine1 = "7550 Maple Street",
+                City = "Orlando",
+                State = "FL",
+                ZipCode = "32789"
+            };
+
+            Assert.ThrowsException<System.ArgumentException>(() => personController.AddPerson(personToAdd));
+        }
+
+        [TestMethod]
+        public void ShouldAddPersonAndSetPersonIdPropertyIfPropertiesAreValid()
+        {
+            Person personToAdd = new Person
+            {
+                LastName = "Duck",
+                FirstName = "Donald",
+                DateOfBirth = new DateTime(1950, 1, 1),
+                SocialSecurityNumber = "444-44-4444",
+                Gender = "M",
+                PhoneNumber = "(333) 333-3333",
+                AddressLine1 = "7550 Maple Street",
+                City = "Orlando",
+                State = "FL",
+                ZipCode = "32789"
+            };
+
+            Assert.AreEqual(default, personToAdd.PersonId);
+            personController.AddPerson(personToAdd);
+            Assert.AreNotEqual(default, personToAdd.PersonId);
+
+            Person personFromDatabase = personController.GetPerson(personToAdd.PersonId);
+            Assert.AreEqual("Duck", personFromDatabase.LastName);
+            Assert.AreEqual("Donald", personFromDatabase.FirstName);
+            Assert.AreEqual(new DateTime(1950, 1, 1), personFromDatabase.DateOfBirth);
+            Assert.AreEqual("444-44-4444", personFromDatabase.SocialSecurityNumber);
+            Assert.AreEqual("M", personFromDatabase.Gender);
+            Assert.AreEqual("(333) 333-3333", personFromDatabase.PhoneNumber);
+            Assert.AreEqual("7550 Maple Street", personFromDatabase.AddressLine1);
+            Assert.AreEqual("Orlando", personFromDatabase.City);
+            Assert.AreEqual("FL", personFromDatabase.State);
+            Assert.AreEqual("32789", personFromDatabase.ZipCode);
+        }
+    }
+}
