3ea3da36608ffff064391c0ba7a1005709c4e9e3
Michael Studard <mstudar1@my.westga.edu>
2021-06-08 17:34:21 -0400

    Added EditPerson method to PersonDAL class

---
+++ ChildcareManagementStudioTests/DAL/PersonDALWhenEditPerson.cs
@@ -0,0 +1,81 @@
+﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ChildcareManagementStudioTests.DAL
+{
+    /// <summary>
+    /// Class for testing the EditPerson method of the PersonDAL class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class PersonDALWhenEditPerson
+    {
+        private PersonDAL personDAL;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            personDAL = new PersonDAL();
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfOriginalPersonIsNull()
+        {
+            
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfRevisedPersonIsNull()
+        {
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfPersonIdIsDifferent()
+        {
+        }
+
+        [TestMethod]
+        public void ShouldUpdateDatabaseIfArgumentsAreValid()
+        {
+            Person originalPerson = new Person
+            {
+                PersonId = 1,
+                LastName = "Mouse",
+                FirstName = "Mickey",
+                DateOfBirth = new DateTime(1930, 1, 1),
+                SocialSecurityNumber = "111-11-1111",
+                Gender = "M",
+                PhoneNumber = "(111) 111-1111",
+                AddressLine1 = "123 Clubhouse Lane",
+                City = "Orlando",
+                State = "FL",
+                ZipCode = "32830"
+            };
+
+            Person revisedPerson = new Person
+            {
+                PersonId = 1,
+                LastName = "Mouse",
+                FirstName = "Mickey",
+                DateOfBirth = new DateTime(1930, 1, 1),
+                SocialSecurityNumber = "111-11-1111",
+                Gender = "M",
+                PhoneNumber = "(111) 111-1111",
+                AddressLine1 = "123 Clubhouse Lane",
+                City = "Lake Buena Vista",
+                State = "FL",
+                ZipCode = "32830"
+            };
+
+            personDAL.EditPerson(originalPerson, revisedPerson);
+
+            Person retrievedPerson = personDAL.GetPerson(originalPerson.PersonId);
+            Assert.AreEqual("Lake Buena Vista", retrievedPerson.City);
+
+            personDAL.EditPerson(revisedPerson, originalPerson);    // this line is intended to undo the change made during the test
+        }
+    }
+}
