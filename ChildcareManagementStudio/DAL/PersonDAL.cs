7d5d9d01bcc954d1d06f901ece3b5483e99317f3
Michael Studard <mstudar1@my.westga.edu>
2021-06-07 20:29:11 -0400

    Added AddPerson method to PersonDAL class

--- ChildcareManagementStudio/DAL/PersonDAL.cs
+++ ChildcareManagementStudio/DAL/PersonDAL.cs
@@ -73,5 +73,80 @@ namespace ChildcareManagementStudio.DAL
 
             return person;
         }
+
+        /// <summary>
+        /// Method that adds a person to the database.
+        /// </summary>
+        /// <param name="person">
+        /// A Person object representing the person to be added to the database.
+        /// For the fields in the database that are set as not null, the object must have corresponding property values.
+        /// Additionally, the Person object being passed to this method cannot have a value for the PersonId property since it is assigned by the database.
+        /// This method completes the PersonId property for the referenced Person object after it is assigned by the database.
+        /// </param>
+        public void AddPerson(Person person)
+        {
+            if (PersonIsMissingMandatoryFields(person))
+            {
+                throw new ArgumentException("The Person object must have all the mandatory properties filled out.", "person");
+            }
+
+            if (person.PersonId != default)
+            {
+                throw new ArgumentException("The PersonId property cannot be filled out because it will be assigned by the database.", "person");
+            }
+
+            string insertStatement =
+                "INSERT INTO Person (lastName, firstName, dateOfBirth, ssn, gender, phoneNumber, addressLine1, addressLine2, city, state, zipCode) " +
+                "VALUES ($lastName, $firstName, $dateOfBirth, $ssn, $gender, $phoneNumber, $addressLine1, $addressLine2, $city, $state, $zipCode)";
+
+            using (SqliteConnection connection = ChildCareDatabaseConnection.GetConnection())
+            {
+                connection.Open();
+
+                using (SqliteCommand insertCommand = new SqliteCommand(insertStatement, connection))
+                {
+                    insertCommand.Parameters.AddWithValue("$lastName", person.LastName);
+                    insertCommand.Parameters.AddWithValue("$firstName", person.FirstName);
+                    insertCommand.Parameters.AddWithValue("$dateOfBirth", person.DateOfBirth);
+                    insertCommand.Parameters.AddWithValue("$ssn", person.SocialSecurityNumber);
+                    insertCommand.Parameters.AddWithValue("$gender", person.Gender);
+                    insertCommand.Parameters.AddWithValue("$phoneNumber", person.PhoneNumber);
+                    insertCommand.Parameters.AddWithValue("$addressLine1", person.AddressLine1);
+                    if (person.AddressLine2 == default)
+                    {
+                        insertCommand.Parameters.AddWithValue("$addressLine2", DBNull.Value);
+                    }
+                    else
+                    {
+                        insertCommand.Parameters.AddWithValue("$addressLine2", person.AddressLine2);
+                    }
+                    insertCommand.Parameters.AddWithValue("$city", person.City);
+                    insertCommand.Parameters.AddWithValue("$state", person.State);
+                    insertCommand.Parameters.AddWithValue("$zipCode", person.ZipCode);
+                    insertCommand.ExecuteNonQuery();
+                }
+
+                using (SqliteCommand selectCommand = new SqliteCommand("SELECT last_insert_rowid()", connection))
+                {
+                    using (SqliteDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            person.PersonId = reader.GetInt32(0);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Helper method that ensures all of the mandatory properties are included with the specified Person object.
+        /// </summary>
+        /// <param name="person">The Person object being evaluated.</param>
+        /// <returns>True if the object is missing mandatory properties, false otherwise.</returns>
+        private bool PersonIsMissingMandatoryFields(Person person)
+        {
+            return person.LastName == default || person.FirstName == default || person.DateOfBirth == default || person.SocialSecurityNumber == default || person.Gender == default || person.PhoneNumber == default || person.PhoneNumber == default || person.AddressLine1 == default || person.City == default || person.State == default || person.ZipCode == default;
+        }
     }
 }
---
+++ ChildcareManagementStudioTests/DAL/PersonDALWhenAddPerson.cs
@@ -0,0 +1,98 @@
+﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ChildcareManagementStudioTests.DAL
+{
+    /// <summary>
+    /// Class for testing the AddPerson method of the PersonDAL class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class PersonDALWhenAddPerson
+    {
+        private PersonDAL personDAL;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            personDAL = new PersonDAL();
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfPersonIsMissingMandatoryProperty()
+        {
+            Person personToAdd = new Person
+            {
+                FirstName = "Donald",
+                DateOfBirth = new DateTime(1950, 1, 1),
+                SocialSecurityNumber = "333-33-3333",
+                Gender = "M",
+                PhoneNumber = "(333) 333-3333",
+                AddressLine1 = "7550 Maple Street",
+                City = "Orlando",
+                State = "FL",
+                ZipCode = "32789"
+            };
+
+            Assert.ThrowsException<System.ArgumentException>(() => personDAL.AddPerson(personToAdd));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfPersonAlreadyHasPersonId()
+        {
+            Person personToAdd = new Person
+            {
+                PersonId = 3,
+                LastName = "Duck",
+                FirstName = "Donald",
+                DateOfBirth = new DateTime(1950, 1, 1),
+                SocialSecurityNumber = "333-33-3333",
+                Gender = "M",
+                PhoneNumber = "(333) 333-3333",
+                AddressLine1 = "7550 Maple Street",
+                City = "Orlando",
+                State = "FL",
+                ZipCode = "32789"
+            };
+
+            Assert.ThrowsException<System.ArgumentException>(() => personDAL.AddPerson(personToAdd));
+        }
+
+        [TestMethod]
+        public void ShouldAddPersonAndSetPersonIdPropertyIfPropertiesAreValid()
+        {
+            Person personToAdd = new Person
+            {
+                LastName = "Duck",
+                FirstName = "Donald",
+                DateOfBirth = new DateTime(1950, 1, 1),
+                SocialSecurityNumber = "333-33-3333",
+                Gender = "M",
+                PhoneNumber = "(333) 333-3333",
+                AddressLine1 = "7550 Maple Street",
+                City = "Orlando",
+                State = "FL",
+                ZipCode = "32789"
+            };
+
+            Assert.AreEqual(default, personToAdd.PersonId);
+            personDAL.AddPerson(personToAdd);
+            Assert.AreNotEqual(default, personToAdd.PersonId);
+
+            Person personFromDatabase = personDAL.GetPerson(personToAdd.PersonId);
+            Assert.AreEqual("Duck", personFromDatabase.LastName);
+            Assert.AreEqual("Donald", personFromDatabase.FirstName);
+            Assert.AreEqual(new DateTime(1950, 1, 1), personFromDatabase.DateOfBirth);
+            Assert.AreEqual("333-33-3333", personFromDatabase.SocialSecurityNumber);
+            Assert.AreEqual("M", personFromDatabase.Gender);
+            Assert.AreEqual("(333) 333-3333", personFromDatabase.PhoneNumber);
+            Assert.AreEqual("7550 Maple Street", personFromDatabase.AddressLine1);
+            Assert.AreEqual("Orlando", personFromDatabase.City);
+            Assert.AreEqual("FL", personFromDatabase.State);
+            Assert.AreEqual("32789", personFromDatabase.ZipCode);
+        }
+    }
+}
