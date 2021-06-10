e8e8a79cfe30a153fe99481dd1e4363a688f8ea2
J. Allen Burton <jburton6@my.westga.edu>
2021-06-10 16:14:29 -0400

    Basic layout of teacher detail userControl

---
+++ ChildcareManagementStudio/UserControls/TeacherViewTeacherDetailUserControl.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace ChildcareManagementStudio.UserControls
+{
+    partial class TeacherViewTeacherDetailUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelDOB = new System.Windows.Forms.Label();
+            this.labelAddress1 = new System.Windows.Forms.Label();
+            this.labelAddress2 = new System.Windows.Forms.Label();
+            this.labelCity = new System.Windows.Forms.Label();
+            this.labelState = new System.Windows.Forms.Label();
+            this.labelZipCode = new System.Windows.Forms.Label();
+            this.buttonReturn = new System.Windows.Forms.Button();
+            this.labelStartDate = new System.Windows.Forms.Label();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelSSN = new System.Windows.Forms.Label();
+            this.labelGender = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFirstName.Location = new System.Drawing.Point(86, 29);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(95, 21);
+            this.labelFirstName.TabIndex = 0;
+            this.labelFirstName.Text = "First Name:";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLastName.Location = new System.Drawing.Point(423, 29);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(97, 21);
+            this.labelLastName.TabIndex = 1;
+            this.labelLastName.Text = "Last Name:";
+            // 
+            // labelDOB
+            // 
+            this.labelDOB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelDOB.AutoSize = true;
+            this.labelDOB.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDOB.Location = new System.Drawing.Point(70, 79);
+            this.labelDOB.Name = "labelDOB";
+            this.labelDOB.Size = new System.Drawing.Size(111, 21);
+            this.labelDOB.TabIndex = 2;
+            this.labelDOB.Text = "Date of Birth:";
+            // 
+            // labelAddress1
+            // 
+            this.labelAddress1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelAddress1.AutoSize = true;
+            this.labelAddress1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAddress1.Location = new System.Drawing.Point(92, 179);
+            this.labelAddress1.Name = "labelAddress1";
+            this.labelAddress1.Size = new System.Drawing.Size(89, 21);
+            this.labelAddress1.TabIndex = 4;
+            this.labelAddress1.Text = "Address 1:";
+            // 
+            // labelAddress2
+            // 
+            this.labelAddress2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelAddress2.AutoSize = true;
+            this.labelAddress2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAddress2.Location = new System.Drawing.Point(92, 229);
+            this.labelAddress2.Name = "labelAddress2";
+            this.labelAddress2.Size = new System.Drawing.Size(89, 21);
+            this.labelAddress2.TabIndex = 5;
+            this.labelAddress2.Text = "Address 2:";
+            // 
+            // labelCity
+            // 
+            this.labelCity.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelCity.AutoSize = true;
+            this.labelCity.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCity.Location = new System.Drawing.Point(136, 279);
+            this.labelCity.Name = "labelCity";
+            this.labelCity.Size = new System.Drawing.Size(45, 21);
+            this.labelCity.TabIndex = 6;
+            this.labelCity.Text = "City:";
+            // 
+            // labelState
+            // 
+            this.labelState.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelState.AutoSize = true;
+            this.labelState.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelState.Location = new System.Drawing.Point(463, 279);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(57, 21);
+            this.labelState.TabIndex = 7;
+            this.labelState.Text = "State:";
+            // 
+            // labelZipCode
+            // 
+            this.labelZipCode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelZipCode.AutoSize = true;
+            this.labelZipCode.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelZipCode.Location = new System.Drawing.Point(97, 329);
+            this.labelZipCode.Name = "labelZipCode";
+            this.labelZipCode.Size = new System.Drawing.Size(84, 21);
+            this.labelZipCode.TabIndex = 8;
+            this.labelZipCode.Text = "Zip Code:";
+            // 
+            // buttonReturn
+            // 
+            this.buttonReturn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tableLayoutPanel1.SetColumnSpan(this.buttonReturn, 3);
+            this.buttonReturn.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReturn.Location = new System.Drawing.Point(187, 389);
+            this.buttonReturn.Name = "buttonReturn";
+            this.buttonReturn.Size = new System.Drawing.Size(364, 35);
+            this.buttonReturn.TabIndex = 23;
+            this.buttonReturn.Text = "Return to Teacher List";
+            this.buttonReturn.UseVisualStyleBackColor = true;
+            // 
+            // labelStartDate
+            // 
+            this.labelStartDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelStartDate.AutoSize = true;
+            this.labelStartDate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStartDate.Location = new System.Drawing.Point(424, 329);
+            this.labelStartDate.Name = "labelStartDate";
+            this.labelStartDate.Size = new System.Drawing.Size(96, 21);
+            this.labelStartDate.TabIndex = 21;
+            this.labelStartDate.Text = "Start Date:";
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPhone.Location = new System.Drawing.Point(51, 129);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(130, 21);
+            this.labelPhone.TabIndex = 3;
+            this.labelPhone.Text = "Phone Number:";
+            // 
+            // labelSSN
+            // 
+            this.labelSSN.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelSSN.AutoSize = true;
+            this.labelSSN.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSSN.Location = new System.Drawing.Point(478, 79);
+            this.labelSSN.Name = "labelSSN";
+            this.labelSSN.Size = new System.Drawing.Size(42, 21);
+            this.labelSSN.TabIndex = 23;
+            this.labelSSN.Text = "SSN:";
+            // 
+            // labelGender
+            // 
+            this.labelGender.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelGender.AutoSize = true;
+            this.labelGender.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGender.Location = new System.Drawing.Point(446, 129);
+            this.labelGender.Name = "labelGender";
+            this.labelGender.Size = new System.Drawing.Size(74, 21);
+            this.labelGender.TabIndex = 24;
+            this.labelGender.Text = "Gender:";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.94936F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.94936F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.06685F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.03444F));
+            this.tableLayoutPanel1.Controls.Add(this.labelFirstName, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelLastName, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelDOB, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelAddress1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelAddress2, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.labelCity, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.labelState, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.labelZipCode, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.buttonReturn, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.labelStartDate, 2, 6);
+            this.tableLayoutPanel1.Controls.Add(this.labelPhone, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelSSN, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelGender, 2, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 77F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 13F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(739, 464);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // TeacherViewTeacherDetailUserControl
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "TeacherViewTeacherDetailUserControl";
+            this.Size = new System.Drawing.Size(745, 470);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelDOB;
+        private System.Windows.Forms.Label labelAddress1;
+        private System.Windows.Forms.Label labelAddress2;
+        private System.Windows.Forms.Label labelCity;
+        private System.Windows.Forms.Label labelState;
+        private System.Windows.Forms.Label labelZipCode;
+        private System.Windows.Forms.Button buttonReturn;
+        private System.Windows.Forms.Label labelStartDate;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelSSN;
+        private System.Windows.Forms.Label labelGender;
+    }
+}
