0b379464b278d01f4b39e48b47d86ac0674a16c5
Michael Studard <mstudar1@my.westga.edu>
2021-06-12 13:34:53 -0400

    Added PositionTypeDAL class with GetAllPositionTypes method

---
+++ ChildcareManagementStudio/DAL/PositionTypeDAL.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudio.DAL
+{
+    /// <summary>
+    /// Class for accessing position type information from the child care database.
+    /// </summary>
+    public class PositionTypeDAL
+    {
+        /// <summary>
+        /// Method that returns a list of all the position types in the database.
+        /// </summary>
+        /// <returns>All of the position types in the database.</returns>
+        public List<string> GetAllPositionTypes()
+        {
+            List<string> positionTypes = new List<string>();
+
+            string selectStatement =
+                "SELECT positionType " +
+                "FROM PositionType";
+
+            using (SqliteConnection connection = ChildCareDatabaseConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqliteCommand selectCommand = new SqliteCommand(selectStatement, connection))
+                {
+                    using (SqliteDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        int positionTypeOrdinal = reader.GetOrdinal("positionType");
+                        while (reader.Read())
+                        {
+                            string currentPositionType = reader.GetString(positionTypeOrdinal);
+                            positionTypes.Add(currentPositionType);
+                        }
+                    }
+                }
+            }
+
+            return positionTypes;
+        }
+    }
+}
