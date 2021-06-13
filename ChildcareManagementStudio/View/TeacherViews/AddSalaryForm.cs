80ce01f18d6394984466702a5e378621c507a643
J. Allen Burton <jburton6@my.westga.edu>
2021-06-13 15:27:13 -0400

    Implemented yes/no dialoge on form submit

--- ChildcareManagementStudio/View/TeacherViews/AddSalaryForm.cs
+++ ChildcareManagementStudio/View/TeacherViews/AddSalaryForm.cs
@@ -1,6 +1,7 @@
 ﻿using ChildcareManagementStudio.Controller;
 using ChildcareManagementStudio.Model;
 using ChildcareManagementStudio.UserControls;
+using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -30,6 +31,7 @@ namespace ChildcareManagementStudio.View.TeacherViews
 
         private void ButtonCancel_Click(object sender, EventArgs e)
         {
+            this.referringUserControl.Enabled = true;
             this.Close();
         }
 
@@ -49,19 +51,52 @@ namespace ChildcareManagementStudio.View.TeacherViews
             }
             else
             {
-                SalaryRecord record = new SalaryRecord
+                string title = "Submit Record Confirmation";
+                string message = "Are you sure you want to add a new record with the following information: \n\n" +
+                    "Rate: $" + this.numericUpDownRate.Value.ToString("F") + "/hour \n" +
+                    "Effective Date: " + this.dateTimePickerEffectiveDate.Value.ToString("d");
+                DialogResult dialogResult = MessageBox.Show(message, title, MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
                 {
-                    Rate = (double)this.numericUpDownRate.Value,
-                    EffectiveDate = this.dateTimePickerEffectiveDate.Value
-                };
+                    this.SubmitNewRecord();
+                    this.referringUserControl.Enabled = true;
+                    this.Close();
+                }
+                
+            }
+
+        }
+
+        /// <summary>
+        /// Submit record based on foem fields and display confirmation dialog if no exceptions
+        /// </summary>
+        private void SubmitNewRecord()
+        {
+            SalaryRecord record = new SalaryRecord
+            {
+                Rate = (double)this.numericUpDownRate.Value,
+                EffectiveDate = this.dateTimePickerEffectiveDate.Value
+            };
+            try
+            {
                 this.salaryController.AddSalaryRecord(this.employeeId, record);
                 string title = "Success";
                 string message = "Record has been added.";
                 MessageBox.Show(message, title);
-                this.referringUserControl.Enabled = true;
-                this.Close();
+            } catch (SqliteException ex)
+            {
+                Console.WriteLine("TEST: " + ex.ErrorCode) ;
+                if (ex.ErrorCode == -2147467259)
+                {
+                    string title = "Duplicate Record Error";
+                    string message = "Error. This record matches anothed record on file for this employee.  " +
+                        "Duplicate records are not allowed.  Please try again.";
+                    MessageBox.Show(message, title);
+                }
             }
-
+            
+            
         }
+
     }
 }
