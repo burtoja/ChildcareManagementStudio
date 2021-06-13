65a8fb31332c9c0de66406ef41c63979f8ff425f
Michael Studard <mstudar1@my.westga.edu>
2021-06-13 14:47:04 -0400

    Added PositionTypeController class with GetAllPositionTypes method

---
+++ ChildcareManagementStudioTests/Controller/PositionTypeControllerWhenGetAllPositionTypes.cs
@@ -0,0 +1,23 @@
+﻿using ChildcareManagementStudio.Controller;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudioTests.Controller
+{
+    /// <summary>
+    /// Class for testing the GetAllPositionTypes method of the PositionTypeController class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class PositionTypeControllerWhenGetAllPositionTypes
+    {
+        [TestMethod]
+        public void ShouldReturnAllPositionTypes()
+        {
+            PositionTypeController positionTypeController = new PositionTypeController();
+            List<string> positionTypes = positionTypeController.GetAllPositionTypes();
+            Assert.AreEqual(9, positionTypes.Count);
+        }
+    }
+}
