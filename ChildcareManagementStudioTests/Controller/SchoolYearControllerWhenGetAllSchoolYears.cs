99e59eb9291a7995fbc31ce4d6f27dc0edf8e250
Michael Studard <mstudar1@my.westga.edu>
2021-06-14 20:25:15 -0400

    Added SchoolYearController class with GetAllSchoolYears method

---
+++ ChildcareManagementStudioTests/Controller/SchoolYearControllerWhenGetAllSchoolYears.cs
@@ -0,0 +1,26 @@
+﻿using ChildcareManagementStudio.Controller;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudioTests.Controller
+{
+    /// <summary>
+    /// Class for testing the GetAllSchoolYears method of the SchoolYearController class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class SchoolYearControllerWhenGetAllSchoolYears
+    {
+        [TestMethod]
+        public void ShouldReturnAllSchoolYears()
+        {
+            SchoolYearController schoolYearController = new SchoolYearController();
+            List<string> schoolYears = schoolYearController.GetAllSchoolYears();
+            Assert.AreEqual(3, schoolYears.Count);
+            Assert.AreEqual("2019-20", schoolYears[0]);
+            Assert.AreEqual("2020-21", schoolYears[1]);
+            Assert.AreEqual("2021-22", schoolYears[2]);
+        }
+    }
+}
