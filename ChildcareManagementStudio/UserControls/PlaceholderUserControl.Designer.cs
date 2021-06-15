70f1c82eaa4a0d6d2d927632dd63b007bbcf3703
J. Allen Burton <jburton6@my.westga.edu>
2021-06-15 07:13:03 -0400

    Created placeholder UC

---
+++ ChildcareManagementStudio/UserControls/PlaceholderUserControl.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace ChildcareManagementStudio.UserControls
+{
+    partial class PlaceholderUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(199, 120);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(376, 38);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Feature Coming Soon...";
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.ForeColor = System.Drawing.Color.Maroon;
+            this.labelDescription.Location = new System.Drawing.Point(202, 177);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(0, 21);
+            this.labelDescription.TabIndex = 1;
+            // 
+            // PlaceholderUserControl
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.labelTitle);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "PlaceholderUserControl";
+            this.Size = new System.Drawing.Size(760, 515);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelDescription;
+    }
+}
---
+++ ChildcareManagementStudio/UserControls/PlaceholderUserControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace ChildcareManagementStudio.UserControls
+{
+    public partial class PlaceholderUserControl : UserControl
+    {
+        /// <summary>
+        /// Constructor for the placeholder UC. 
+        /// </summary>
+        /// <param name="messageCode">The description of the upcoming feature to be displayed on the UC</param>
+        public PlaceholderUserControl(string description)
+        {
+            InitializeComponent();
+            this.labelDescription.Text = description;
+        }
+    }
+}
