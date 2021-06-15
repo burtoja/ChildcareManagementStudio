8139915808fcd02bacd5d2467a84aa565708163d
Michael Studard <mstudar1@my.westga.edu>
2021-06-14 20:19:51 -0400

    Added SchoolYearDAL class with GetAllSchoolYears method

---
+++ ChildcareManagementStudioTests/DAL/SchoolYearDALWhenGetAllSchoolYears.cs
@@ -0,0 +1,26 @@
+﻿using ChildcareManagementStudio.DAL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudioTests.DAL
+{
+    /// <summary>
+    /// Class for testing the GetAllSchoolYears method of the SchoolYearDAL class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class SchoolYearDALWhenGetAllSchoolYears
+    {
+        [TestMethod]
+        public void ShouldReturnAllSchoolYears()
+        {
+            SchoolYearDAL schoolYearDAL = new SchoolYearDAL();
+            List<string> schoolYears = schoolYearDAL.GetAllSchoolYears();
+            Assert.AreEqual(3, schoolYears.Count);
+            Assert.AreEqual("2019-20", schoolYears[0]);
+            Assert.AreEqual("2020-21", schoolYears[1]);
+            Assert.AreEqual("2021-22", schoolYears[2]);
+        }
+    }
+}
