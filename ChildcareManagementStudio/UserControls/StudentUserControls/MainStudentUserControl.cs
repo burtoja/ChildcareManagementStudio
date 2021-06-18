745ebc71b3922bbc7e4b51e2b40014a5bd03ffdc
J. Allen Burton <jburton6@my.westga.edu>
2021-06-17 21:51:27 -0400

    Added UC to the main student tab page

--- ChildcareManagementStudio/UserControls/StudentUserControls/MainStudentUserControl.cs
+++ ChildcareManagementStudio/UserControls/StudentUserControls/MainStudentUserControl.cs
@@ -5,6 +5,7 @@ namespace ChildcareManagementStudio.UserControls.StudentUserControls
     public partial class MainStudentUserControl : UserControl
     {
         private readonly ViewStudentListUserControl viewStudentListUserControl;
+        private readonly AddStudentUserControl addStudentUserControl;
 
         /// <summary>
         /// Constructor for the control
@@ -13,6 +14,7 @@ namespace ChildcareManagementStudio.UserControls.StudentUserControls
         {
             InitializeComponent();
             this.viewStudentListUserControl = new ViewStudentListUserControl(this);
+            this.addStudentUserControl = new AddStudentUserControl(this);
             this.AddUserControls();
         }
 
@@ -22,6 +24,7 @@ namespace ChildcareManagementStudio.UserControls.StudentUserControls
         private void AddUserControls()
         {
             this.tabViewAllStudents.Controls.Add(this.viewStudentListUserControl);
+            this.tabAddStudent.Controls.Add(this.addStudentUserControl);
         }
 
         /// <summary>
