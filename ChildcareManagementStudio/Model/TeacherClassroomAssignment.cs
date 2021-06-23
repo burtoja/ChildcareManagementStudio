2c35ac9e3a9f5d276ac9d7d78e9d7dfa3bbfbb24
Michael Studard <mstudar1@my.westga.edu>
2021-06-23 13:30:11 -0400

    Added TeacherClassroomAssignment class

---
+++ ChildcareManagementStudio/Model/TeacherClassroomAssignment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class that records the assignment of a teacher to a classroom.
+    /// </summary>
+    public class TeacherClassroomAssignment
+    {
+        /// <summary>
+        /// Property representing the teacher associated with the record.
+        /// </summary>
+        public Employee Teacher { get; set; }
+
+        /// <summary>
+        /// Property representing the classroom associated with the record.
+        /// </summary>
+        public Classroom Classroom { get; set; }
+
+        /// <summary>
+        /// Property representing the start date of the assignment.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Property recording the school year that the assignment is valid for.
+        /// </summary>
+        public string SchoolYear { get; set; }
+
+        /// <summary>
+        /// Property representing the position type for the assignment.
+        /// </summary>
+        public string PositionType { get; set; }
+    }
+}
