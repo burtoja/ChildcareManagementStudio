15f8fc14e97631045ce63a21676353a41e3d155a
Michael Studard <mstudar1@my.westga.edu>
2021-06-23 13:53:38 -0400

    Added ClassroomController class with GetClassroom method

---
+++ ChildcareManagementStudio/Controller/ClassroomController.cs
@@ -0,0 +1,37 @@
+﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using System;
+
+namespace ChildcareManagementStudio.Controller
+{
+    /// <summary>
+    /// Controller class for managing classrooms.
+    /// </summary>
+    public class ClassroomController
+    {
+        private readonly ClassroomDAL classroomDAL;
+
+        /// <summary>
+        /// Constructor for the ClassroomController class.
+        /// </summary>
+        public ClassroomController()
+        {
+            classroomDAL = new ClassroomDAL();
+        }
+
+        /// <summary>
+        /// Method that returns a Classroom object representing the requested classroom.
+        /// </summary>
+        /// <param name="location">Location of the classroom being requested.</param>
+        /// <returns>A Classroom object representing the requested classroom.</returns>
+        public Classroom GetClassroom(string location)
+        {
+            if (string.IsNullOrEmpty(location))
+            {
+                throw new ArgumentNullException("location", "The classroom location cannot be null.");
+            }
+
+            return classroomDAL.GetClassroom(location);
+        }
+    }
+}
---
+++ ChildcareManagementStudioTests/Controller/ClassroomControllerWhenGetClassroom.cs
@@ -0,0 +1,53 @@
+﻿using ChildcareManagementStudio.Controller;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChildcareManagementStudioTests.Controller
+{
+    /// <summary>
+    /// Class for testing the GetClassroom method of the ClassroomController class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class ClassroomControllerWhenGetClassroom
+    {
+        private ClassroomController classroomController;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            classroomController = new ClassroomController();
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfLocationIsEmpty()
+        {
+            string location = "";
+            Assert.ThrowsException<System.ArgumentNullException>(() => classroomController.GetClassroom(location));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfLocationIsNull()
+        {
+            string location = null;
+            Assert.ThrowsException<System.ArgumentNullException>(() => classroomController.GetClassroom(location));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfLocationIsClassroomIsNotInDatabase()
+        {
+            string location = "Z-101";
+            Assert.ThrowsException<System.ArgumentException>(() => classroomController.GetClassroom(location));
+        }
+
+        [TestMethod]
+        public void ShouldReturnClassroomObjectIfClassroomExists()
+        {
+            string location = "A-101";
+            Classroom classroom = classroomController.GetClassroom(location);
+            Assert.AreEqual("A-101", classroom.Location);
+            Assert.AreEqual(30, classroom.Capacity);
+        }
+    }
+}
