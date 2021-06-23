5df52c46fa7b367d46e5f883ff5abd61b49a058d
J. Allen Burton <jburton6@my.westga.edu>
2021-06-23 09:43:53 -0400

    Add new classroom form layout complete

---
+++ ChildcareManagementStudio/View/ClassroomViews/AddNewClassroomForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace ChildcareManagementStudio.View.ClassroomViews
+{
+    partial class AddNewClassroomForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSchoolYear = new System.Windows.Forms.Label();
+            this.labelClassroomLocation = new System.Windows.Forms.Label();
+            this.comboBoxSchoolYear = new System.Windows.Forms.ComboBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.comboBoxLocation = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // labelSchoolYear
+            // 
+            this.labelSchoolYear.AutoSize = true;
+            this.labelSchoolYear.Location = new System.Drawing.Point(71, 24);
+            this.labelSchoolYear.Name = "labelSchoolYear";
+            this.labelSchoolYear.Size = new System.Drawing.Size(106, 21);
+            this.labelSchoolYear.TabIndex = 0;
+            this.labelSchoolYear.Text = "School Year:";
+            // 
+            // labelClassroomLocation
+            // 
+            this.labelClassroomLocation.AutoSize = true;
+            this.labelClassroomLocation.Location = new System.Drawing.Point(12, 74);
+            this.labelClassroomLocation.Name = "labelClassroomLocation";
+            this.labelClassroomLocation.Size = new System.Drawing.Size(165, 21);
+            this.labelClassroomLocation.TabIndex = 1;
+            this.labelClassroomLocation.Text = "Classroom Location:";
+            // 
+            // comboBoxSchoolYear
+            // 
+            this.comboBoxSchoolYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSchoolYear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxSchoolYear.FormattingEnabled = true;
+            this.comboBoxSchoolYear.Location = new System.Drawing.Point(183, 21);
+            this.comboBoxSchoolYear.Name = "comboBoxSchoolYear";
+            this.comboBoxSchoolYear.Size = new System.Drawing.Size(191, 29);
+            this.comboBoxSchoolYear.TabIndex = 2;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(16, 123);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(161, 33);
+            this.buttonSubmit.TabIndex = 4;
+            this.buttonSubmit.Text = "Create Class";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(211, 123);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(161, 33);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxLocation
+            // 
+            this.comboBoxLocation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxLocation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxLocation.FormattingEnabled = true;
+            this.comboBoxLocation.Location = new System.Drawing.Point(183, 71);
+            this.comboBoxLocation.Name = "comboBoxLocation";
+            this.comboBoxLocation.Size = new System.Drawing.Size(214, 29);
+            this.comboBoxLocation.TabIndex = 3;
+            // 
+            // AddNewClassroomForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(435, 187);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.comboBoxLocation);
+            this.Controls.Add(this.comboBoxSchoolYear);
+            this.Controls.Add(this.labelClassroomLocation);
+            this.Controls.Add(this.labelSchoolYear);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddNewClassroomForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add New Classroom Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSchoolYear;
+        private System.Windows.Forms.Label labelClassroomLocation;
+        private System.Windows.Forms.ComboBox comboBoxSchoolYear;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ComboBox comboBoxLocation;
+    }
+}
