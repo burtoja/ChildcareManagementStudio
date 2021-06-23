b592756de7c7fd08d2b0ac51c939a97f5576b410
Michael Studard <mstudar1@my.westga.edu>
2021-06-23 13:48:50 -0400

    Added ClassroomDAL class with GetClassroom method

---
+++ ChildcareManagementStudioTests/DAL/ClassroomDALWhenGetClassroom.cs
@@ -0,0 +1,53 @@
+﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChildcareManagementStudioTests.DAL
+{
+    /// <summary>
+    /// Class for testing the GetClassroom method of the ClassroomDAL class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class ClassroomDALWhenGetClassroom
+    {
+        private ClassroomDAL classroomDAL;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            classroomDAL = new ClassroomDAL();
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfLocationIsEmpty()
+        {
+            string location = "";
+            Assert.ThrowsException<System.ArgumentNullException>(() => classroomDAL.GetClassroom(location));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfLocationIsNull()
+        {
+            string location = null;
+            Assert.ThrowsException<System.ArgumentNullException>(() => classroomDAL.GetClassroom(location));
+        }
+
+        [TestMethod]
+        public void ShouldThrowExceptionIfLocationIsClassroomIsNotInDatabase()
+        {
+            string location = "Z-101";
+            Assert.ThrowsException<System.ArgumentException>(() => classroomDAL.GetClassroom(location));
+        }
+
+        [TestMethod]
+        public void ShouldReturnClassroomObjectIfClassroomExists()
+        {
+            string location = "A-101";
+            Classroom classroom = classroomDAL.GetClassroom(location);
+            Assert.AreEqual("A-101", classroom.Location);
+            Assert.AreEqual(30, classroom.Capacity);
+        }
+    }
+}
