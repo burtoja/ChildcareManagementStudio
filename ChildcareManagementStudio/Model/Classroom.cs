9902585c3dc34749ae4c3131d1757f1438723877
Michael Studard <mstudar1@my.westga.edu>
2021-06-23 13:28:29 -0400

    Added Classroom class

---
+++ ChildcareManagementStudio/Model/Classroom.cs
@@ -0,0 +1,18 @@
+﻿namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class representing a classroom.
+    /// </summary>
+    public class Classroom
+    {
+        /// <summary>
+        /// Property recording the location of the classroom.
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Property recording the number of students that the classroom can accommodate.
+        /// </summary>
+        public int Capacity { get; set; }
+    }
+}
