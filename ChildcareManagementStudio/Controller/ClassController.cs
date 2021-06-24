59a771bb09d7b44b39143cfcf5ecd5c4dbd1059b
J. Allen Burton <jburton6@my.westga.edu>
2021-06-24 15:55:32 -0400

    Stubbed in base methods to ClassController

--- ChildcareManagementStudio/Controller/ClassController.cs
+++ ChildcareManagementStudio/Controller/ClassController.cs
@@ -22,6 +22,11 @@ namespace ChildcareManagementStudio.Controller
             //this.classDAL = new ClassDAL();
         }
 
+        public void AddClass(Class theClass)
+        {
+            //TODO:  Implement this method when DAL complete
+        }
+
         /// <summary>
         /// Get the class by ClassId
         /// </summary>
@@ -29,7 +34,7 @@ namespace ChildcareManagementStudio.Controller
         /// <returns></returns>
         public Class GetClass(int classId) 
         {
-            // TODO: implement when DAL complete (and remove demoClass
+            // TODO: Implement when DAL complete (and remove demoClass)
             Class demoClass = new Class
             {
                 ClassId = 2,
@@ -37,6 +42,37 @@ namespace ChildcareManagementStudio.Controller
                 SchoolYear = "2021-22"
             };
             return demoClass;
+        }
+
+        /// <summary>
+        /// Returns a list of Class objects matching the provided school year
+        /// </summary>
+        /// <param name="schoolYear">the school year for which to return a list of Class objects</param>
+        /// <returns></returns>
+        public List<Class> GetAllClassesForSchoolYear(string schoolYear)
+        {
+            // TODO: Implement when DAL complete (and remove demoList)
+            Class demoClass1 = new Class
+            {
+                ClassId = 1,
+                ClassroomLocation = "Multi-purpose Room",
+                SchoolYear = "2021-22"
+            };
+            Class demoClass2 = new Class
+            {
+                ClassId = 2,
+                ClassroomLocation = "Room 5A",
+                SchoolYear = "2021-22"
+            };
+            List<Class> demoList = new List<Class>();
+            demoList.Add(demoClass1);
+            demoList.Add(demoClass2);
+            return demoList;
+        }
+
+        public void EditClass(Class originalClass, Class revisedClass)
+        {
+            // TODO: Implement when DAL complete
         }
 
 
