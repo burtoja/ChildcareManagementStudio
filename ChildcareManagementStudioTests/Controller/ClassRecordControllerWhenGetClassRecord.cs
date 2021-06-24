e4885ac4c7ff548d7096b342a155ee50c621d761
Michael Studard <mstudar1@my.westga.edu>
2021-06-24 17:24:16 -0400

    Added ClassRecordController class with GetClassRecord method

---
+++ ChildcareManagementStudioTests/Controller/ClassRecordControllerWhenGetClassRecord.cs
@@ -0,0 +1,24 @@
+﻿using ChildcareManagementStudio.Controller;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChildcareManagementStudioTests.Controller
+{
+    /// <summary>
+    /// Class for testing the GetClassRecord method of the ClassRecordController class.
+    /// </summary>
+    [TestClass]
+    public class ClassRecordControllerWhenGetClassRecord
+    {
+        [TestMethod]
+        public void ShouldReturnClassRecord()
+        {
+            ClassRecordController classRecordController = new ClassRecordController();
+            int classId = 1;
+            ClassRecord classRecord = classRecordController.GetClassRecord(classId);
+            Assert.AreEqual(1, classRecord.ClassId);
+            Assert.AreEqual("A-101", classRecord.Classroom.Location);
+            Assert.AreEqual("2021-22", classRecord.SchoolYear);
+        }
+    }
+}
