3fe1d98854da042d109f6d5d4ccbcd585743ad56
J. Allen Burton <jburton6@my.westga.edu>
2021-06-24 09:45:08 -0400

    Revised forms to treat classroom as physical room

--- ChildcareManagementStudio/UserControls/ClassroomUserControls/ViewClassroomListUserControl.Designer.cs
+++ ChildcareManagementStudio/UserControls/ClassroomUserControls/ViewClassroomListUserControl.Designer.cs
@@ -31,10 +31,8 @@ namespace ChildcareManagementStudio.UserControls.ClassroomUserControls
         {
             this.listViewAllStudents = new System.Windows.Forms.ListView();
             this.location = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.numberOfStudents = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.schoolYear = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.buttonAddNewClassroom = new System.Windows.Forms.Button();
-            this.buttonViewSelectedClassroomDetails = new System.Windows.Forms.Button();
+            this.capacity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonCreateNewClassroom = new System.Windows.Forms.Button();
             this.buttonEditSelectedClassroom = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -44,8 +42,7 @@ namespace ChildcareManagementStudio.UserControls.ClassroomUserControls
             this.listViewAllStudents.AccessibleName = "Student List";
             this.listViewAllStudents.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.location,
-            this.numberOfStudents,
-            this.schoolYear});
+            this.capacity});
             this.listViewAllStudents.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listViewAllStudents.FullRowSelect = true;
             this.listViewAllStudents.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
@@ -63,60 +60,42 @@ namespace ChildcareManagementStudio.UserControls.ClassroomUserControls
             this.location.Text = "Classroom Location";
             this.location.Width = 248;
             // 
-            // numberOfStudents
+            // capacity
             // 
-            this.numberOfStudents.Text = "Number of Students Enrolled";
-            this.numberOfStudents.Width = 244;
+            this.capacity.Text = "Capacity";
+            this.capacity.Width = 244;
             // 
-            // schoolYear
+            // buttonCreateNewClassroom
             // 
-            this.schoolYear.Text = "School Year";
-            this.schoolYear.Width = 206;
-            // 
-            // buttonAddNewClassroom
-            // 
-            this.buttonAddNewClassroom.AccessibleDescription = "";
-            this.buttonAddNewClassroom.AccessibleName = "";
-            this.buttonAddNewClassroom.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonAddNewClassroom.Location = new System.Drawing.Point(19, 408);
-            this.buttonAddNewClassroom.Name = "buttonAddNewClassroom";
-            this.buttonAddNewClassroom.Size = new System.Drawing.Size(182, 43);
-            this.buttonAddNewClassroom.TabIndex = 5;
-            this.buttonAddNewClassroom.Text = "Add New Classroom";
-            this.buttonAddNewClassroom.UseVisualStyleBackColor = true;
-            this.buttonAddNewClassroom.Click += new System.EventHandler(this.ButtonAddNewClassroom_Click);
-            // 
-            // buttonViewSelectedClassroomDetails
-            // 
-            this.buttonViewSelectedClassroomDetails.AccessibleDescription = "";
-            this.buttonViewSelectedClassroomDetails.AccessibleName = "";
-            this.buttonViewSelectedClassroomDetails.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonViewSelectedClassroomDetails.Location = new System.Drawing.Point(221, 408);
-            this.buttonViewSelectedClassroomDetails.Name = "buttonViewSelectedClassroomDetails";
-            this.buttonViewSelectedClassroomDetails.Size = new System.Drawing.Size(304, 43);
-            this.buttonViewSelectedClassroomDetails.TabIndex = 6;
-            this.buttonViewSelectedClassroomDetails.Text = "View Selected Classroom Details";
-            this.buttonViewSelectedClassroomDetails.UseVisualStyleBackColor = true;
+            this.buttonCreateNewClassroom.AccessibleDescription = "";
+            this.buttonCreateNewClassroom.AccessibleName = "";
+            this.buttonCreateNewClassroom.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCreateNewClassroom.Location = new System.Drawing.Point(19, 408);
+            this.buttonCreateNewClassroom.Name = "buttonCreateNewClassroom";
+            this.buttonCreateNewClassroom.Size = new System.Drawing.Size(350, 43);
+            this.buttonCreateNewClassroom.TabIndex = 5;
+            this.buttonCreateNewClassroom.Text = "Create New Classroom";
+            this.buttonCreateNewClassroom.UseVisualStyleBackColor = true;
+            this.buttonCreateNewClassroom.Click += new System.EventHandler(this.ButtonAddNewClassroom_Click);
             // 
             // buttonEditSelectedClassroom
             // 
             this.buttonEditSelectedClassroom.AccessibleDescription = "";
             this.buttonEditSelectedClassroom.AccessibleName = "";
             this.buttonEditSelectedClassroom.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonEditSelectedClassroom.Location = new System.Drawing.Point(544, 408);
+            this.buttonEditSelectedClassroom.Location = new System.Drawing.Point(376, 408);
             this.buttonEditSelectedClassroom.Name = "buttonEditSelectedClassroom";
-            this.buttonEditSelectedClassroom.Size = new System.Drawing.Size(182, 43);
+            this.buttonEditSelectedClassroom.Size = new System.Drawing.Size(350, 43);
             this.buttonEditSelectedClassroom.TabIndex = 7;
-            this.buttonEditSelectedClassroom.Text = "Add New Classroom";
+            this.buttonEditSelectedClassroom.Text = "Edit Selected Classroom";
             this.buttonEditSelectedClassroom.UseVisualStyleBackColor = true;
             // 
             // ViewClassroomListUserControl
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.Controls.Add(this.buttonEditSelectedClassroom);
-            this.Controls.Add(this.buttonViewSelectedClassroomDetails);
             this.Controls.Add(this.listViewAllStudents);
-            this.Controls.Add(this.buttonAddNewClassroom);
+            this.Controls.Add(this.buttonCreateNewClassroom);
             this.Name = "ViewClassroomListUserControl";
             this.Size = new System.Drawing.Size(745, 470);
             this.ResumeLayout(false);
@@ -127,10 +106,8 @@ namespace ChildcareManagementStudio.UserControls.ClassroomUserControls
 
         private System.Windows.Forms.ListView listViewAllStudents;
         private System.Windows.Forms.ColumnHeader location;
-        private System.Windows.Forms.ColumnHeader numberOfStudents;
-        private System.Windows.Forms.ColumnHeader schoolYear;
-        private System.Windows.Forms.Button buttonAddNewClassroom;
-        private System.Windows.Forms.Button buttonViewSelectedClassroomDetails;
+        private System.Windows.Forms.ColumnHeader capacity;
+        private System.Windows.Forms.Button buttonCreateNewClassroom;
         private System.Windows.Forms.Button buttonEditSelectedClassroom;
     }
 }
--- ChildcareManagementStudio/View/ClassroomViews/AddNewClassroomForm.Designer.cs
+++ ChildcareManagementStudio/View/ClassroomViews/AddNewClassroomForm.Designer.cs
@@ -29,49 +29,28 @@ namespace ChildcareManagementStudio.View.ClassroomViews
         /// </summary>
         private void InitializeComponent()
         {
-            this.labelSchoolYear = new System.Windows.Forms.Label();
             this.labelClassroomLocation = new System.Windows.Forms.Label();
-            this.comboBoxSchoolYear = new System.Windows.Forms.ComboBox();
             this.buttonSubmit = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
-            this.comboBoxLocation = new System.Windows.Forms.ComboBox();
             this.labelCapacity = new System.Windows.Forms.Label();
             this.numericUpDownCapacity = new System.Windows.Forms.NumericUpDown();
             this.labelCapcityUnits = new System.Windows.Forms.Label();
+            this.textBoxLocation = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCapacity)).BeginInit();
             this.SuspendLayout();
-            // 
-            // labelSchoolYear
-            // 
-            this.labelSchoolYear.AutoSize = true;
-            this.labelSchoolYear.Location = new System.Drawing.Point(71, 24);
-            this.labelSchoolYear.Name = "labelSchoolYear";
-            this.labelSchoolYear.Size = new System.Drawing.Size(106, 21);
-            this.labelSchoolYear.TabIndex = 0;
-            this.labelSchoolYear.Text = "School Year:";
             // 
             // labelClassroomLocation
             // 
             this.labelClassroomLocation.AutoSize = true;
-            this.labelClassroomLocation.Location = new System.Drawing.Point(12, 74);
+            this.labelClassroomLocation.Location = new System.Drawing.Point(12, 19);
             this.labelClassroomLocation.Name = "labelClassroomLocation";
             this.labelClassroomLocation.Size = new System.Drawing.Size(165, 21);
             this.labelClassroomLocation.TabIndex = 1;
             this.labelClassroomLocation.Text = "Classroom Location:";
             // 
-            // comboBoxSchoolYear
-            // 
-            this.comboBoxSchoolYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxSchoolYear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBoxSchoolYear.FormattingEnabled = true;
-            this.comboBoxSchoolYear.Location = new System.Drawing.Point(183, 21);
-            this.comboBoxSchoolYear.Name = "comboBoxSchoolYear";
-            this.comboBoxSchoolYear.Size = new System.Drawing.Size(191, 29);
-            this.comboBoxSchoolYear.TabIndex = 1;
-            // 
             // buttonSubmit
             // 
-            this.buttonSubmit.Location = new System.Drawing.Point(26, 189);
+            this.buttonSubmit.Location = new System.Drawing.Point(28, 108);
             this.buttonSubmit.Name = "buttonSubmit";
             this.buttonSubmit.Size = new System.Drawing.Size(161, 33);
             this.buttonSubmit.TabIndex = 4;
@@ -81,7 +60,7 @@ namespace ChildcareManagementStudio.View.ClassroomViews
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(236, 189);
+            this.buttonCancel.Location = new System.Drawing.Point(223, 108);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(161, 33);
             this.buttonCancel.TabIndex = 5;
@@ -89,20 +68,10 @@ namespace ChildcareManagementStudio.View.ClassroomViews
             this.buttonCancel.UseVisualStyleBackColor = true;
             this.buttonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
             // 
-            // comboBoxLocation
-            // 
-            this.comboBoxLocation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxLocation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBoxLocation.FormattingEnabled = true;
-            this.comboBoxLocation.Location = new System.Drawing.Point(183, 71);
-            this.comboBoxLocation.Name = "comboBoxLocation";
-            this.comboBoxLocation.Size = new System.Drawing.Size(214, 29);
-            this.comboBoxLocation.TabIndex = 2;
-            // 
             // labelCapacity
             // 
             this.labelCapacity.AutoSize = true;
-            this.labelCapacity.Location = new System.Drawing.Point(89, 126);
+            this.labelCapacity.Location = new System.Drawing.Point(89, 63);
             this.labelCapacity.Name = "labelCapacity";
             this.labelCapacity.Size = new System.Drawing.Size(88, 21);
             this.labelCapacity.TabIndex = 6;
@@ -110,7 +79,7 @@ namespace ChildcareManagementStudio.View.ClassroomViews
             // 
             // numericUpDownCapacity
             // 
-            this.numericUpDownCapacity.Location = new System.Drawing.Point(183, 124);
+            this.numericUpDownCapacity.Location = new System.Drawing.Point(183, 63);
             this.numericUpDownCapacity.Maximum = new decimal(new int[] {
             50,
             0,
@@ -125,32 +94,37 @@ namespace ChildcareManagementStudio.View.ClassroomViews
             // 
             this.labelCapcityUnits.AutoSize = true;
             this.labelCapcityUnits.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelCapcityUnits.Location = new System.Drawing.Point(247, 130);
+            this.labelCapcityUnits.Location = new System.Drawing.Point(247, 69);
             this.labelCapcityUnits.Name = "labelCapcityUnits";
             this.labelCapcityUnits.Size = new System.Drawing.Size(61, 16);
             this.labelCapcityUnits.TabIndex = 8;
             this.labelCapcityUnits.Text = "students";
             // 
+            // textBoxLocation
+            // 
+            this.textBoxLocation.Location = new System.Drawing.Point(183, 19);
+            this.textBoxLocation.Name = "textBoxLocation";
+            this.textBoxLocation.Size = new System.Drawing.Size(240, 27);
+            this.textBoxLocation.TabIndex = 9;
+            // 
             // AddNewClassroomForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(435, 253);
+            this.ClientSize = new System.Drawing.Size(435, 161);
+            this.Controls.Add(this.textBoxLocation);
             this.Controls.Add(this.labelCapcityUnits);
             this.Controls.Add(this.numericUpDownCapacity);
             this.Controls.Add(this.labelCapacity);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonSubmit);
-            this.Controls.Add(this.comboBoxLocation);
-            this.Controls.Add(this.comboBoxSchoolYear);
             this.Controls.Add(this.labelClassroomLocation);
-            this.Controls.Add(this.labelSchoolYear);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AddNewClassroomForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Add New Classroom Form";
+            this.Text = "Create New Classroom Form";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCapacity)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -158,15 +132,12 @@ namespace ChildcareManagementStudio.View.ClassroomViews
         }
 
         #endregion
-
-        private System.Windows.Forms.Label labelSchoolYear;
         private System.Windows.Forms.Label labelClassroomLocation;
-        private System.Windows.Forms.ComboBox comboBoxSchoolYear;
         private System.Windows.Forms.Button buttonSubmit;
         private System.Windows.Forms.Button buttonCancel;
-        private System.Windows.Forms.ComboBox comboBoxLocation;
         private System.Windows.Forms.Label labelCapacity;
         private System.Windows.Forms.NumericUpDown numericUpDownCapacity;
         private System.Windows.Forms.Label labelCapcityUnits;
+        private System.Windows.Forms.TextBox textBoxLocation;
     }
 }
