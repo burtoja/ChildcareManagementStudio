f139482a19bd20c617af84b6f1f7a834343471d4
J. Allen Burton <jburton6@my.westga.edu>
2021-06-24 09:05:56 -0400

    Cleaned up add classroom form and added comments

--- ChildcareManagementStudio/View/ClassroomViews/AddNewClassroomForm.cs
+++ ChildcareManagementStudio/View/ClassroomViews/AddNewClassroomForm.cs
@@ -1,27 +1,33 @@
 ﻿using ChildcareManagementStudio.UserControls.ClassroomUserControls;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ChildcareManagementStudio.View.ClassroomViews
 {
+    /// <summary>
+    /// Date	Client	Activity	Start Time	End Time	Billable Hours	Rate (/hr)	Total	Balance	Notes:																
+    /// </summary>
     public partial class AddNewClassroomForm : Form
     {
-        ViewClassroomListUserControl referringUserControl;
+        private readonly ViewClassroomListUserControl referringUserControl;
 
+
+        /// <summary>
+        /// Constructor for the form
+        /// </summary>
+        /// <param name="referringUserControl">the userControl creating this form</param>
         public AddNewClassroomForm(ViewClassroomListUserControl referringUserControl)
         {
             InitializeComponent();
             this.referringUserControl = referringUserControl;
         }
 
-        private void buttonCancel_Click(object sender, EventArgs e)
+        /// <summary>
+        /// handles cance button clicks to close form and re-enable the referring UC
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonCancel_Click(object sender, EventArgs e)
         {
             this.referringUserControl.Enabled = true;
             this.Close();
