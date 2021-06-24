b94cdead02c38ce463b8d6aec1c76d939295d5b8
J. Allen Burton <jburton6@my.westga.edu>
2021-06-24 09:07:39 -0400

    Added comments

--- ChildcareManagementStudio/UserControls/ClassroomUserControls/ViewClassroomListUserControl.cs
+++ ChildcareManagementStudio/UserControls/ClassroomUserControls/ViewClassroomListUserControl.cs
@@ -1,23 +1,27 @@
 ﻿using ChildcareManagementStudio.View.ClassroomViews;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ChildcareManagementStudio.UserControls.ClassroomUserControls
 {
+    /// <summary>
+    /// Creates and manages the UC for viewing and managing the list of active classroom records
+    /// </summary>
     public partial class ViewClassroomListUserControl : UserControl
     {
+        /// <summary>
+        /// Constructor for the UC
+        /// </summary>
         public ViewClassroomListUserControl()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Handles Add New Classroom button clicks to open the appropriate form and disable this UC
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ButtonAddNewClassroom_Click(object sender, EventArgs e)
         {
             AddNewClassroomForm addNewClassroomForm = new AddNewClassroomForm(this);
