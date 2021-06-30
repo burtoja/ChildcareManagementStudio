367c0e347ff5de7718252a82d5876ce280834bbc
Michael Studard <mstudar1@my.westga.edu>
2021-06-29 21:07:34 -0400

    Removed StartDate and PositionType properties from TeacherClassroomAssignment class

--- ChildcareManagementStudio/Model/TeacherClassroomAssignment.cs
+++ ChildcareManagementStudio/Model/TeacherClassroomAssignment.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace ChildcareManagementStudio.Model
+﻿namespace ChildcareManagementStudio.Model
 {
     /// <summary>
     /// Class that records the assignment of a teacher to a classroom.
@@ -16,15 +14,5 @@ namespace ChildcareManagementStudio.Model
         /// Property representing the class record associated with the record.
         /// </summary>
         public ClassRecord ClassRecord { get; set; }
-
-        /// <summary>
-        /// Property representing the start date of the assignment.
-        /// </summary>
-        public DateTime StartDate { get; set; }
-
-        /// <summary>
-        /// Property representing the position type for the assignment.
-        /// </summary>
-        public string PositionType { get; set; }
     }
 }
