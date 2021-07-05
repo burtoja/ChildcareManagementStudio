2ca81e6b55a7887dcac2d940fb248cd0b1f8d79b
J. Allen Burton <jburton6@my.westga.edu>
2021-07-05 07:52:04 -0400

    View payment UC layout created

---
+++ ChildcareManagementStudio/UserControls/FinancialUserControls/ViewPaymentsUserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChildcareManagementStudio.UserControls.FinancialUserControls
+{
+    public partial class ViewPaymentsUserControl : UserControl
+    {
+        public ViewPaymentsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
