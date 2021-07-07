2a050a75c3fd9549de2a97e4ffff48b899dffa41
Michael Studard <mstudar1@my.westga.edu>
2021-07-07 18:07:54 -0400

    Added classroom capacity to ShouldAddValidRecord method of StudentClassroomAssignmentDALWhenDeleteStudentClassroomAssignment class

--- ChildcareManagementStudioTests/DAL/StudentClassroomAssignmentDALWhenDeleteStudentClassroomAssignment.cs
+++ ChildcareManagementStudioTests/DAL/StudentClassroomAssignmentDALWhenDeleteStudentClassroomAssignment.cs
@@ -34,9 +34,15 @@ namespace ChildcareManagementStudioTests.DAL
                 StudentId = 1
             };
 
+            Classroom classroom = new Classroom()
+            {
+                Capacity = 30
+            };
+
             ClassRecord classRecord = new ClassRecord()
             {
-                ClassId = 1
+                ClassId = 1,
+                Classroom = classroom
             };
 
             StudentClassroomAssignment studentClassroomAssignment = new StudentClassroomAssignment()
