a3334566ecb7909c5c2a62fe661dbb30e8c8b544
Michael Studard <mstudar1@my.westga.edu>
2021-07-07 19:25:15 -0400

    Added Payment class

---
+++ ChildcareManagementStudio/Model/Payment.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class representing a payment made by an account holder.
+    /// </summary>
+    public class Payment
+    {
+        /// <summary>
+        /// The account holder making the payment.
+        /// </summary>
+        public AccountHolder AccountHolder { get; set; }
+
+        /// <summary>
+        /// The unique ID of the payment.
+        /// </summary>
+        public int PaymentId { get; set; }
+
+        /// <summary>
+        /// The date the payment was made.
+        /// </summary>
+        public DateTime PaymentDate { get; set; }
+
+        /// <summary>
+        /// The amount of the payment, expressed in US dollars.
+        /// A positive number indicates a payment.
+        /// A negative number indicates a refund.
+        /// </summary>
+        public double Amount { get; set; }
+
+        /// <summary>
+        /// The method of payment that the account holder used for this payment.
+        /// </summary>
+        public PaymentType PaymentType { get; set; }
+    }
+
+    /// <summary>
+    /// Enumeration for recording a form of payment.
+    /// </summary>
+    public enum PaymentType
+    {
+        Cash,
+        Check,
+        CreditCard
+    }
+}
