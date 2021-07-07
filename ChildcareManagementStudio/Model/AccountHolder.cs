8e2649f5c93f88aa9947995525c86c82c5f177c0
Michael Studard <mstudar1@my.westga.edu>
2021-07-07 19:17:38 -0400

    Added AccountHolder class

---
+++ ChildcareManagementStudio/Model/AccountHolder.cs
@@ -0,0 +1,13 @@
+﻿namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class representing an account holder.
+    /// </summary>
+    public class AccountHolder : Person
+    {
+        /// <summary>
+        /// The account holder's account hoder ID.
+        /// </summary>
+        public int AccountHolderId { get; set; }
+    }
+}
