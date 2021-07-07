227beae7927cf513dcbf0f69399ce71aa2104a16
Michael Studard <mstudar1@my.westga.edu>
2021-07-07 19:33:48 -0400

    Added GrossPay class

---
+++ ChildcareManagementStudio/Model/GrossPay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class used to communicate the amount of gross pay
+    /// that an employee is owed within a date range.
+    /// </summary>
+    public class GrossPay
+    {
+        /// <summary>
+        /// The employee that is owed the pay.
+        /// </summary>
+        public Employee Employee { get; set; }
+
+        /// <summary>
+        /// The start date used to calculate the gross pay owed.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// The end date used to calculate the gross pay owed.
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// The amount of gross pay that the employee is owed.
+        /// </summary>
+        public double Amount { get; set; }
+    }
+}
