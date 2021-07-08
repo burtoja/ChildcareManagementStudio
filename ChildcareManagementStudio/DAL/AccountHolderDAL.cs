08ba7f82bfe8eb3a9e262ef04df994475265568a
Michael Studard <mstudar1@my.westga.edu>
2021-07-07 20:24:15 -0400

    Added AccountHolderDAL class with GetAllAccountHolders method

---
+++ ChildcareManagementStudio/DAL/AccountHolderDAL.cs
@@ -0,0 +1,65 @@
+﻿using ChildcareManagementStudio.Model;
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudio.DAL
+{
+    /// <summary>
+    /// Class for accessing account holder information from the child care database.
+    /// </summary>
+    public class AccountHolderDAL : PersonDAL
+    {
+        /// <summary>
+        /// Method that returns AccountHolder objects for all of the account holders in the database.
+        /// </summary>
+        /// <returns>A list of AccountHolder objects for all of the account holders in the database.</returns>
+        public List<AccountHolder> GetAllAccountHolders()
+        {
+            List<AccountHolder> accountHolders = new List<AccountHolder>();
+
+            string selectStatement =
+                "SELECT ah.personId, ah.accountHolderId " +
+                "FROM AccountHolder ah " +
+                "JOIN Person p ON ah.personId = p.personId " +
+                "ORDER BY p.lastName, p.firstName";
+
+            using (SqliteConnection connection = ChildCareDatabaseConnection.GetConnection())
+            {
+                connection.Open();
+                using (SqliteCommand selectCommand = new SqliteCommand(selectStatement, connection))
+                {
+                    using (SqliteDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        int personIdOrdinal = reader.GetOrdinal("personId");
+                        int accountHolderIdOrdinal = reader.GetOrdinal("accountHolderId");
+                        while (reader.Read())
+                        {
+                            AccountHolder accountHolder = new AccountHolder
+                            {
+                                PersonId = reader.GetInt32(personIdOrdinal),
+                                AccountHolderId = reader.GetInt32(accountHolderIdOrdinal)
+                            };
+
+                            Person person = GetPerson(accountHolder.PersonId);
+                            accountHolder.LastName = person.LastName;
+                            accountHolder.FirstName = person.FirstName;
+                            accountHolder.DateOfBirth = person.DateOfBirth;
+                            accountHolder.SocialSecurityNumber = person.SocialSecurityNumber;
+                            accountHolder.Gender = person.Gender;
+                            accountHolder.PhoneNumber = person.PhoneNumber;
+                            accountHolder.AddressLine1 = person.AddressLine1;
+                            if (person.AddressLine2 != default) { accountHolder.AddressLine2 = person.AddressLine2; }
+                            accountHolder.City = person.City;
+                            accountHolder.State = person.State;
+                            accountHolder.ZipCode = person.ZipCode;
+
+                            accountHolders.Add(accountHolder);
+                        }
+                    }
+                }
+            }
+
+            return accountHolders;
+        }
+    }
+}
---
+++ ChildcareManagementStudioTests/DAL/AccountHolderDALWhenGetAllAccountHolders.cs
@@ -0,0 +1,55 @@
+﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudioTests.DAL
+{
+    /// <summary>
+    /// Class for testing the GetAllAccountHolders method of the AccountHolderDAL class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class AccountHolderDALWhenGetAllAccountHolders
+    {
+        [TestMethod]
+        public void ShouldReturnAllEmployees()
+        {
+            AccountHolderDAL accountHolderDAL = new AccountHolderDAL();
+
+            List<AccountHolder> accountHolders = accountHolderDAL.GetAllAccountHolders();
+            Assert.AreEqual(2, accountHolders.Count);
+
+            AccountHolder firstAccountHolder = accountHolders[1];
+            Assert.AreEqual(101, firstAccountHolder.PersonId);
+            Assert.AreEqual("Woodward", firstAccountHolder.LastName);
+            Assert.AreEqual("Ryan", firstAccountHolder.FirstName);
+            Assert.AreEqual(new DateTime(2020, 2, 14), firstAccountHolder.DateOfBirth);
+            Assert.AreEqual("880-45-5899", firstAccountHolder.SocialSecurityNumber);
+            Assert.AreEqual("M", firstAccountHolder.Gender);
+            Assert.AreEqual("(613) 418-1082", firstAccountHolder.PhoneNumber);
+            Assert.AreEqual("P.O. Box 156, 2014 Placerat. Av.", firstAccountHolder.AddressLine1);
+            Assert.AreEqual("Bhilwara", firstAccountHolder.City);
+            Assert.AreEqual("VA", firstAccountHolder.State);
+            Assert.AreEqual("88249", firstAccountHolder.ZipCode);
+            Assert.AreEqual(1, firstAccountHolder.AccountHolderId);
+
+            AccountHolder secondAccountHolder = accountHolders[0];
+            Assert.AreEqual(102, secondAccountHolder.PersonId);
+            Assert.AreEqual("Holmes", secondAccountHolder.LastName);
+            Assert.AreEqual("Ishmael", secondAccountHolder.FirstName);
+            Assert.AreEqual(new DateTime(2014, 3, 1), secondAccountHolder.DateOfBirth);
+            Assert.AreEqual("332-83-3496", secondAccountHolder.SocialSecurityNumber);
+            Assert.AreEqual("F", secondAccountHolder.Gender);
+            Assert.AreEqual("(140) 550-5520", secondAccountHolder.PhoneNumber);
+            Assert.AreEqual("7460 Libero Av.", secondAccountHolder.AddressLine1);
+            Assert.AreEqual("Apartment 691", secondAccountHolder.AddressLine2);
+            Assert.AreEqual("Kohlu", secondAccountHolder.City);
+            Assert.AreEqual("VA", secondAccountHolder.State);
+            Assert.AreEqual("91990", secondAccountHolder.ZipCode);
+            Assert.AreEqual(2, secondAccountHolder.AccountHolderId);
+        }
+    }
+}
