bdd69ad0d20d6364355ac2f2310abc1079145572
J. Allen Burton <jburton6@my.westga.edu>
2021-07-08 08:53:09 -0400

    Removed seconds from test asserts since app DB is not recording to that level of precision (by design)

--- ChildcareManagementStudioTests/DAL/ClockRecordDALWhenClockIn.cs
+++ ChildcareManagementStudioTests/DAL/ClockRecordDALWhenClockIn.cs
@@ -8,24 +8,30 @@ namespace ChildcareManagementStudioTests.DAL
     [TestClass]
     public class ClockRecordDALWhenClockIn
     {
+        private ClockRecordDAL clockRecordDAL;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.clockRecordDAL = new ClockRecordDAL();
+        }
         [TestMethod]
         public void ShouldAddNewRecordWithInDateTimeForEmployee()
         {
-            ClockRecordDAL clockRecordDAL = new ClockRecordDAL();
-            List<ClockRecord> recordsBefore = clockRecordDAL.GetAllClockRecordsForEmployee(1);
+            List<ClockRecord> recordsBefore = this.clockRecordDAL.GetAllClockRecordsForEmployee(1);
             Assert.AreEqual(1, recordsBefore.Count);
             ClockRecord testRecord = new ClockRecord()
             {
                 EmployeeId = 1,
-                InDateTime = new System.DateTime(2021, 7, 6, 9, 30, 15)
-            };           
-            clockRecordDAL.ClockIn(testRecord);
-            List<ClockRecord> recordsAfter = clockRecordDAL.GetAllClockRecordsForEmployee(1);
+                InDateTime = new System.DateTime(2021, 7, 6, 9, 30, 00)
+            };
+            this.clockRecordDAL.ClockIn(testRecord);
+            List<ClockRecord> recordsAfter = this.clockRecordDAL.GetAllClockRecordsForEmployee(1);
             Assert.AreEqual(2, recordsAfter.Count);
             Assert.AreEqual("7/5/2021 8:00:00 AM", recordsAfter[0].InDateTime.ToString());
             Assert.AreEqual("7/5/2021 5:00:00 PM", recordsAfter[0].OutDateTime.ToString());
-            Assert.AreEqual("7/6/2021 9:30:15 AM", recordsAfter[1].InDateTime.ToString());
-            clockRecordDAL.DeleteClockRecord(testRecord);
+            Assert.AreEqual("7/6/2021 9:30:00 AM", recordsAfter[1].InDateTime.ToString());
+            this.clockRecordDAL.DeleteClockRecord(testRecord);
         }
     }
 }
