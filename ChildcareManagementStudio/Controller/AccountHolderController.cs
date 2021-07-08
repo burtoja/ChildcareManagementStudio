61379803ce7939c5fe81fd879861700a6bbd84af
Michael Studard <mstudar1@my.westga.edu>
2021-07-07 21:18:15 -0400

    Added AcountHolderController class with GetAllAccountHolders method

---
+++ ChildcareManagementStudio/Controller/AccountHolderController.cs
@@ -0,0 +1,31 @@
+﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudio.Controller
+{
+    /// <summary>
+    /// Controller class for account holders.
+    /// </summary>
+    public class AccountHolderController
+    {
+        private readonly AccountHolderDAL accountHolderDAL;
+
+        /// <summary>
+        /// Constructor for the AccountHolderController class.
+        /// </summary>
+        public AccountHolderController()
+        {
+            accountHolderDAL = new AccountHolderDAL();
+        }
+
+        /// <summary>
+        /// Method that returns AccountHolder objects for all of the account holders in the database.
+        /// </summary>
+        /// <returns>A list of AccountHolder objects for all of the account holders in the database.</returns>
+        public List<AccountHolder> GetAllAccountHolders()
+        {
+            return accountHolderDAL.GetAllAccountHolders();
+        }
+    }
+}
