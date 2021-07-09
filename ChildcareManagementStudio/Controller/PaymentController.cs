1000253d0eddfd8144550aca2cd3501b3d7f4bd3
Michael Studard <mstudar1@my.westga.edu>
2021-07-08 21:03:56 -0400

    Added PaymentController class with GetPayments method

---
+++ ChildcareManagementStudio/Controller/PaymentController.cs
@@ -0,0 +1,38 @@
+﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using System;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudio.Controller
+{
+    /// <summary>
+    /// Controller class for payments.
+    /// </summary>
+    public class PaymentController
+    {
+        private readonly PaymentDAL paymentDAL;
+
+        /// <summary>
+        /// Constructor for the PaymentController class.
+        /// </summary>
+        public PaymentController()
+        {
+            paymentDAL = new PaymentDAL();
+        }
+
+        /// <summary>
+        /// Method that returns all of the payments made by the specified account holder.
+        /// </summary>
+        /// <param name="accountHolder">The account holder in question.</param>
+        /// <returns>A list of Payment objects representing all of the payments made by the specified account holder.</returns>
+        public List<Payment> GetPayments(AccountHolder accountHolder)
+        {
+            if (accountHolder == null)
+            {
+                throw new ArgumentNullException("accountHolder", "The account holder cannot be null.");
+            }
+
+            return paymentDAL.GetPayments(accountHolder);
+        }
+    }
+}
---
+++ ChildcareManagementStudioTests/Controller/PaymentControllerWhenGetPayments.cs
@@ -0,0 +1,35 @@
+﻿using ChildcareManagementStudio.Controller;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudioTests.Controller
+{
+    /// <summary>
+    /// Class for testing the GetPayments method of the PaymentController class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class PaymentControllerWhenGetPayments
+    {
+        [TestMethod]
+        public void ShouldReturnAllPayments()
+        {
+            PaymentController paymentController = new PaymentController();
+            AccountHolder accountHolder = new AccountHolder()
+            {
+                AccountHolderId = 1
+            };
+            List<Payment> payments = paymentController.GetPayments(accountHolder);
+            Payment firstPayment = payments[0];
+
+            Assert.AreEqual(1, payments.Count);
+            Assert.AreEqual(1, firstPayment.PaymentId);
+            Assert.AreEqual(new DateTime(2021, 7, 7), firstPayment.PaymentDate);
+            Assert.AreEqual(100.00, firstPayment.Amount);
+            Assert.AreEqual(PaymentType.CreditCard, firstPayment.PaymentType);
+        }
+    }
+}
