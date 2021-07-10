0f0c53639d2e495188ef5496d9332b7a744adad3
Michael Studard <mstudar1@my.westga.edu>
2021-07-10 11:09:53 -0400

    Added GetBalance method to BalanceController class

--- ChildcareManagementStudio/Controller/BalanceController.cs
+++ ChildcareManagementStudio/Controller/BalanceController.cs
@@ -1,4 +1,6 @@
 ﻿using ChildcareManagementStudio.DAL;
+using ChildcareManagementStudio.Model;
+using System.Collections.Generic;
 
 namespace ChildcareManagementStudio.Controller
 {
@@ -19,6 +21,20 @@ namespace ChildcareManagementStudio.Controller
             accountHolderDAL = new AccountHolderDAL();
             tuitionRateRecordDAL = new TuitionRateRecordDAL();
             paymentDAL = new PaymentDAL();
+        }
+
+        /// <summary>
+        /// Method that gets the balance for the specified account holder.
+        /// </summary>
+        /// <param name="accountHolder">The account holder.</param>
+        /// <returns>The balance for the specified account holder.</returns>
+        public Balance GetBalance(AccountHolder accountHolder)
+        {
+            List<TuitionRateRecord> tuitionRateRecords = tuitionRateRecordDAL.GetTuitionRateRecords(accountHolder);
+            List<Payment> payments = paymentDAL.GetPayments(accountHolder);
+            BalanceCalculator balanceCalculator = new BalanceCalculator(tuitionRateRecords, payments);
+            Balance balance = balanceCalculator.GetBalance();
+            return balance;
         }
     }
 }
---
+++ ChildcareManagementStudioTests/Controller/BalanceControllerWhenGetBalance.cs
@@ -0,0 +1,33 @@
+﻿using ChildcareManagementStudio.Controller;
+using ChildcareManagementStudio.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChildcareManagementStudioTests.Controller
+{
+    /// <summary>
+    /// Class for testing the GetBalance method of the BalanceController class.
+    /// Note that this is only a temporary test class.
+    /// These tests are based on the current state of the production database.
+    /// </summary>
+    [TestClass]
+    public class BalanceControllerWhenGetBalance
+    {
+        [TestMethod]
+        public void ShouldGetBalance()
+        {
+            //double expectedBalanceAmount = 1285;
+
+            AccountHolder accountHolder = new AccountHolder()
+            {
+                AccountHolderId = 1
+            };
+
+            BalanceController balanceController = new BalanceController();
+
+            Balance balance = balanceController.GetBalance(accountHolder);
+
+            Assert.AreEqual(accountHolder.AccountHolderId, balance.AccountHolder.AccountHolderId);
+            //Assert.AreEqual(expectedBalanceAmount, balance.Amount);
+        }
+    }
+}
