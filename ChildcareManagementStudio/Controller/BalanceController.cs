8b2d868fd82b94aeafec28f6b95ce56ad10b2871
Michael Studard <mstudar1@my.westga.edu>
2021-07-10 10:59:08 -0400

    Added BalanceController class

---
+++ ChildcareManagementStudio/Controller/BalanceController.cs
@@ -0,0 +1,24 @@
+﻿using ChildcareManagementStudio.DAL;
+
+namespace ChildcareManagementStudio.Controller
+{
+    /// <summary>
+    /// Controller class for getting balances.
+    /// </summary>
+    public class BalanceController
+    {
+        private readonly AccountHolderDAL accountHolderDAL;
+        private readonly TuitionRateRecordDAL tuitionRateRecordDAL;
+        private readonly PaymentDAL paymentDAL;
+
+        /// <summary>
+        /// Constructor for the BalanceController class.
+        /// </summary>
+        public BalanceController()
+        {
+            accountHolderDAL = new AccountHolderDAL();
+            tuitionRateRecordDAL = new TuitionRateRecordDAL();
+            paymentDAL = new PaymentDAL();
+        }
+    }
+}
