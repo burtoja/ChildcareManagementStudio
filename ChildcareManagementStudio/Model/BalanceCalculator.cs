bcaff319256d653b8482bb02364f4fe6997a560a
Michael Studard <mstudar1@my.westga.edu>
2021-07-09 20:43:13 -0400

    Added BalanceCalculator class.

---
+++ ChildcareManagementStudio/Model/BalanceCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChildcareManagementStudio.Model
+{
+    /// <summary>
+    /// Class that can be used to calculate an account holder's balance,
+    /// given a list of tuition rate records and a list of payments.
+    /// </summary>
+    public class BalanceCalculator
+    {
+        private List<TuitionRateRecord> tuitionRateRecords;
+        private List<Payment> payments;
+
+        /// <summary>
+        /// Constructor for the BalanceCalculator class.
+        /// </summary>
+        /// <param name="tuitionRateRecords">A list of tuition rate records associated with the account holder.</param>
+        /// <param name="payments">A list of payments associated with the account holder.</param>
+        public BalanceCalculator(List<TuitionRateRecord> tuitionRateRecords, List<Payment> payments)
+        {
+            if (tuitionRateRecords == null)
+            {
+                throw new ArgumentNullException("tuitionRateRecords", "The tuition rate records cannot be null.");
+            }
+
+            if (payments == null)
+            {
+                throw new ArgumentNullException("payments", "The payments cannot be null.");
+            }
+
+            this.tuitionRateRecords = tuitionRateRecords;
+            this.payments = payments;
+        }
+    }
+}
