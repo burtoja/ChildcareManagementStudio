68c8411833120dfca30c0548c4da2e5f0415cbf2
Michael Studard <mstudar1@my.westga.edu>
2021-07-10 11:47:44 -0400

    Added code to show balances

--- ChildcareManagementStudio/UserControls/FinancialUserControls/ViewBalancesUserControl.cs
+++ ChildcareManagementStudio/UserControls/FinancialUserControls/ViewBalancesUserControl.cs
@@ -1,20 +1,44 @@
-﻿using System;
+﻿using ChildcareManagementStudio.Controller;
+using ChildcareManagementStudio.Model;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ChildcareManagementStudio.UserControls.FinancialUserControls
 {
+    /// <summary>
+    /// User control that allows users to view all non-zero account holder balances.
+    /// </summary>
     public partial class ViewBalancesUserControl : UserControl
     {
+        private BalanceController balanceController;
+        private List<Balance> balances;
+
+        /// <summary>
+        /// Constructor for the user control.
+        /// </summary>
         public ViewBalancesUserControl()
         {
             InitializeComponent();
+            balanceController = new BalanceController();
+            this.RefreshBalanceListView();
+        }
+
+        /// <summary>
+        /// Method that refreshes the list of balances.
+        /// </summary>
+        private void RefreshBalanceListView()
+        {
+            this.listViewBalances.Items.Clear();
+            balances = balanceController.GetAllBalances();
+            foreach (Balance currentBalance in balances)
+            {
+                if (currentBalance.Amount != 0)
+                {
+                    ListViewItem item = new ListViewItem(currentBalance.AccountHolder.FullName);
+                    item.SubItems.Add(currentBalance.Amount.ToString("$#,##0.00"));
+                    this.listViewBalances.Items.Add(item);
+                }
+            }
         }
     }
 }
